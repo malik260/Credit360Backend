68a2fe190cc7b1421b50aebc0d0f36dc67d0f184
Nwaka Augustine <augustinee@gmail.com>
2017-07-28 18:16:54 +0100

    Controller Update

--- FintrakBanking.ViewModels/Credit/CollateralViewModel.cs
+++ FintrakBanking.ViewModels/Credit/CollateralViewModel.cs
@@ -249,7 +249,7 @@ namespace FintrakBanking.ViewModels.Credit
         public int collateralCustomerId { get; set; }
         public string documentCategory { get; set; }
         public string documentRef { get; set; }
-        public int documentCode { get; set; }
+        public string documentCode { get; set; }
         public string documentType { get; set; }
         public bool isMandatory { get; set; }
         public string remark { get; set; }
