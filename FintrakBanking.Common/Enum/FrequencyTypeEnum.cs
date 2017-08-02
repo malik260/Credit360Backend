23b1379bd2e13ee45729f9bf32b9d41034034993
Administrator <anu.omotayo@fintraksoftware.com>
2017-08-02 10:08:08 +0100

    loan schedule

---
+++ FintrakBanking.Common/Enum/FrequencyTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace FintrakBanking.Common.Enum
+{
+    public enum FrequencyTypeEnum { Yearly = 1, TwiceYearly = 2, Quarterly = 3, SixTimesYearly = 4, Monthly = 5,
+                                    TwiceMonthly = 6, Weekly = 7, Daily = 8, ThriceYearly = 9};
+
+}
+
+//1	Yearly
+//2	Twice-Yearly
+//3	Quarterly
+//4	Six- times-Yearly
+//5	Monthly
+//6	Twice-Monthly
+//7	Weekly
+//8	Daily
+//9	Thrice-Yearly
+
