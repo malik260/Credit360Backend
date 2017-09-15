2a54ecb64879e422a951f5f8a267aba4d58a242c
developerslearnit <developerslearnit@gmail.com>
2017-09-15 15:20:40 +0100

    state collateral

--- FintrakBanking.ViewModels/Setups/General/StateViewModel.cs
+++ FintrakBanking.ViewModels/Setups/General/StateViewModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace FintrakBanking.ViewModels.Setups.General
 {
-    public class StateViewModel
+    public class StateViewModel : GeneralEntity
     {
         public int StateId { get; set; }
         public int CountryId { get; set; }
         public string StateName { get; set; }
+        public string CountryName { get; set; }
+        public decimal CollateralSearchChargeAmount { get; set; }
     }
 }
