b44cb549cebd24dcd53549dfa8f46b03dde82604
Gbenro <olugbenro.selere@fintraksoftware.com>
2017-09-25 18:02:22 +0100

    fixed errors on specified files

--- FintrakBanking.Entities/Models/tbl_Approval_Trail.cs
+++ FintrakBanking.Entities/Models/tbl_Approval_Trail.cs
@@ -45,8 +45,6 @@ namespace FintrakBanking.Entities.Models
         [StringLength(700)]
         public string Comment { get; set; }
 
-        public bool VotedYes { get; set; }
-
         public virtual tbl_Approval_Level tbl_Approval_Level { get; set; }
 
         public virtual tbl_Approval_Level tbl_Approval_Level1 { get; set; }
