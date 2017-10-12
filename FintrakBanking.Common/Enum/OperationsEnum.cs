30ece0a93fd63566200d502738b34b8d6760b670
Osemeke <osemekeanyirah@fintraksoftware.com>
2017-10-12 21:57:13 +0100

    value

--- FintrakBanking.Common/Enum/OperationsEnum.cs
+++ FintrakBanking.Common/Enum/OperationsEnum.cs
@@ -42,8 +42,7 @@ namespace FintrakBanking.Common.Enum
         CompleteWriteOff = 31,
         CancelUndisbursedLoan = 32,
         InterestSuspension = 33,
-        CollateralRelease = 35
-        InterestSuspension = 33,
+        CollateralRelease = 35,
         LoanTermination = 34
 
 
