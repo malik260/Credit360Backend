6d96373ae59db0e746f8a84cb1e277ea557956c6
Gbenro Selere <seleregb@gmail.com>
2017-11-10 11:00:15 +0100

    update

--- FintrakBanking.ViewModels/Credit/LoanViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanViewModel.cs
@@ -326,7 +326,6 @@ namespace FintrakBanking.ViewModels.Credit
         public string camReference { get; set; }
         public List<CasaViewModel> customerAccounts { get; set; }
         public int appraisalMemorandumId { get; set; }
-        public string customerCode { get; set; }
         public int? casaAccountId { get; set; }
         public string loanStatusName { get; set; }
         public string sectorName { get; set; }
