27b0174385d70279b28e63a64c5c366205497084
gbenga.olufemi <gbenga.olufemi@fintraksoftware.com>
2017-12-28 09:09:08 +0100

    update OfferLetter

--- FintrakBanking.ViewModels/Credit/LoanViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanViewModel.cs
@@ -387,7 +387,7 @@ namespace FintrakBanking.ViewModels.Credit
         public List<LoanCollateralMappingViewModel> loanCollateral { get; set; }
         public CustomerCompanyInfomationViewModels companyInformation { get; set; }
         public List<CamDocumentViewModel> camDocuments { get; set; }
-        public short productClassProcessId { get; set; }
+        public short? productClassProcessId { get; set; }
 
         //......End f Loan Relational Table View Mapping Models......//
     }
@@ -574,6 +574,7 @@ namespace FintrakBanking.ViewModels.Credit
         public string applicationReferenceNumber { get; set; }
         public short applicationStatusId { get; set; }
         public int? toStaffId { get; set; }
+        public short? productClassId { get; set; }
 
     }
 
