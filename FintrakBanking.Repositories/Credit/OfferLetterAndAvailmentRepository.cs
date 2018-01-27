5ab5378b0b613f9a6ea283887e6c630a16558a0e
augustine <augustine.nwaka@fintraksoftware.com>
2018-01-27 18:28:15 +0100

    update

--- FintrakBanking.Repositories/Credit/OfferLetterAndAvailmentRepository.cs
+++ FintrakBanking.Repositories/Credit/OfferLetterAndAvailmentRepository.cs
@@ -496,7 +496,6 @@ namespace FintrakBanking.Repositories.Credit
                                        }).GroupBy(x => x.conditionPrecident).Select(y => y.FirstOrDefault()).ToList();
 
             var conditionSubsequents = (from a in context.TBL_LOAN_APPLICATION
-                                        join b in context.TBL_LOAN_CONDITION_PRECEDENT on a.LOANAPPLICATIONID equals b.LOANAPPLICATIONID
                                         join c in context.TBL_LOAN_APPLICATION_DETAIL on a.LOANAPPLICATIONID equals c.LOANAPPLICATIONID
                                         join b in context.TBL_LOAN_CONDITION_PRECEDENT on a.LOANAPPLICATIONID equals b.TBL_LOAN_APPLICATION_DETAIL.LOANAPPLICATIONID
                                         where a.APPLICATIONREFERENCENUMBER == applicationRefNumber && b.ISSUBSEQUENT == true
