502efc99c7aac23888cb202feac87376fbe250e6
developerslearnit <developerslearnit@gmail.com>
2018-02-09 16:52:18 +0100

    commit

---
+++ FintrakBanking.Common/Enum/CustomerInformationTrackerEnum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.Common.Enum
+{
+    public enum CustomerInformationTrackerEnum
+    {
+        General_Information = 1,
+        Corporate_Information = 2,
+        Address_Addition = 3,
+        Address_Modification = 4,
+        Phone_Number_Addition = 5,
+        Phone_Number_Modification = 6,
+        Director_Addition = 7,
+        Director_Modification = 8,
+        Director_Removal = 9,
+        Signatory_Adition = 10,
+        Signatory_Modification = 11,
+        Signatory_Removal = 12,
+        Client_Addition = 13,
+        Client_Modification = 14,
+        Client_Removal = 15,
+        Supplier_Addition = 16,
+        Suplier_Modification = 17,
+        Supplier_Removal = 18,
+        Shareholder_Addition = 19,
+        Shareholder_Modification = 20,
+        Shareholder_Removal = 21,
+        Shareholder_Ultimate_Beneficiary_Addition = 22,
+        Shareholder_Ultimate_Beneficiary_Modification = 23,
+        Shareholder_Ultimate_Beneficiary_Removal = 24,
+        Employement_History_Addition = 25,
+        Employment_History_Modification = 26,
+        Next_of_Kin_Addition = 27,
+        Next_of_Kin_Modification = 28,
+        Customer_Children_Addition = 29,
+        Customer_Children_Modification = 30,
+        Document_Addition = 31
+    }
+}
