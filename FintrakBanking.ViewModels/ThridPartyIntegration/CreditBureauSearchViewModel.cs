41a18b6401192f4fed5e2303cf96065e7306989c
maurer ogbonnnaya <maurer@live.co.uk>
2018-02-09 17:36:51 +0100

    update XDS integration

--- FintrakBanking.ViewModels/ThridPartyIntegration/CreditBureauSearchViewModel.cs
+++ FintrakBanking.ViewModels/ThridPartyIntegration/CreditBureauSearchViewModel.cs
@@ -9,7 +9,30 @@ namespace FintrakBanking.ViewModels.ThridPartyIntegration
     public  class CreditBureauSearchViewModel
     {
     }
-    public class IndividualSearchViewModel
+
+    public class CreditBureauIndividualSearchViewModel
+    {
+        public short creditBureauId { get; set; }
+        public string enquiryReason { get; set; }
+        public string customerName { get; set; }
+        public string dateOfBirth { get; set; }
+        public string gender { get; set; }
+        public string identificationNumber { get; set; }
+        public string accountNumber { get; set; }
+        public string productID { get; set; }
+    }
+
+    public class CreditBureauCorporateSearchViewModel
+    {
+        public short creditBureauId { get; set; }
+        public string enquiryReason { get; set; }
+        public string customerName { get; set; }
+        public string businessRegistrationNumber { get; set; }
+        public string accountNumber { get; set; }
+        public string productID { get; set; }
+    }
+
+    public class XDSIndividualSearchViewModel
     {
         public string DataTicket { get; set; }
         public string EnquiryReason { get; set; }
@@ -20,4 +43,22 @@ namespace FintrakBanking.ViewModels.ThridPartyIntegration
         public string ProductID { get; set; }
     }
 
+    public class XDSCommercialSearchViewModel
+    {
+        public string DataTicket { get; set; }
+        public string EnquiryReason { get; set; }
+        public string BusinessName { get; set; }
+        public string BusinessRegistrationNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string ProductID { get; set; }
+    }
+
+    public class SearchFullResultViewModel
+    {
+        public string DataTicket { get; set; }
+        public int ConsumerID { get; set; }
+        public string MergeList { get; set; }
+        public string SubscriberEnquiryEngineID { get; set; }
+        public int EnquiryID { get; set; }
+    }
 }
