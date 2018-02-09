3b6b76dd4713bc0bfae99f94729947a306cac4ba
maurer ogbonnnaya <maurer@live.co.uk>
2018-02-09 11:19:00 +0100

    XDS credit bureau integration with the api

---
+++ FintrakBanking.ViewModels/ThridPartyIntegration/CreditBureauSearchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.ViewModels.ThridPartyIntegration
+{
+    public  class CreditBureauSearchViewModel
+    {
+    }
+    public class IndividualSearchViewModel
+    {
+        public string DataTicket { get; set; }
+        public string EnquiryReason { get; set; }
+        public string ConsumerName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Identification { get; set; }
+        public string AccountNumber { get; set; }
+        public string ProductID { get; set; }
+    }
+
+}
