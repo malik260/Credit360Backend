06dab9fd16f218e5778f8d3cdbc27aadedb218ed
Osemeke <osemekeanyirah@fintraksoftware.com>
2018-02-22 14:49:32 +0100

    merge conflict resolution

--- FintrakBanking.Entities/Models/tbl_Loan_Application_Detail.cs
+++ FintrakBanking.Entities/Models/tbl_Loan_Application_Detail.cs
@@ -69,10 +69,6 @@ namespace FintrakBanking.Entities.Models
         [Required]
         [StringLength(500)]
         public string LOANPURPOSE { get; set; }
-        [StringLength(500)]
-        public string REPAYMENTTERMS { get; set; }
-        [StringLength(500)]
-        public string REPAYMENTSCHEDULE { get; set; }
 
         public short CONSESSIONAPPROVALSTATUSID { get; set; }
 
