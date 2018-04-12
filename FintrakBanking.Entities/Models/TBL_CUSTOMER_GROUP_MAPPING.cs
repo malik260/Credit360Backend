abf69f49994b55bb55c312fb2b5904e2cf17ab2a
Osemeke <osemekeanyirah@fintraksoftware.com>
2018-04-12 19:32:19 +0100

    scaffold

---
+++ FintrakBanking.Entities/Models/TBL_CUSTOMER_GROUP_MAPPING.cs
@@ -0,0 +1,41 @@
+namespace FintrakBanking.Entities.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("core.TBL_CUSTOMER_GROUP_MAPPING")]
+    public partial class TBL_CUSTOMER_GROUP_MAPPING
+    {
+        [Key]
+        public int CUSTOMERGROUPMAPPINGID { get; set; }
+
+        public int CUSTOMERID { get; set; }
+
+        public int CUSTOMERGROUPID { get; set; }
+
+        public short RELATIONSHIPTYPEID { get; set; }
+
+        public int CREATEDBY { get; set; }
+
+        public int? LASTUPDATEDBY { get; set; }
+
+        public DateTime DATETIMECREATED { get; set; }
+
+        public DateTime? DATETIMEUPDATED { get; set; }
+
+        public bool? DELETED { get; set; }
+
+        public int? DELETEDBY { get; set; }
+
+        public DateTime? DATETIMEDELETED { get; set; }
+
+        public virtual TBL_CUSTOMER TBL_CUSTOMER { get; set; }
+
+        public virtual TBL_CUSTOMER_GROUP TBL_CUSTOMER_GROUP { get; set; }
+
+        public virtual TBL_CUSTOMER_GROUP_RELATN_TYPE TBL_CUSTOMER_GROUP_RELATN_TYPE { get; set; }
+    }
+}
---
+++ FintrakBanking.Entities/Models/TBL_REGION.cs
@@ -0,0 +1,32 @@
+namespace FintrakBanking.Entities.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("core.TBL_REGION")]
+    public partial class TBL_REGION
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TBL_REGION()
+        {
+            TBL_STATE = new HashSet<TBL_STATE>();
+        }
+
+        [Key]
+        public int REGIONID { get; set; }
+
+        public int COUNTRYID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string REGIONNAME { get; set; }
+
+        public virtual TBL_COUNTRY TBL_COUNTRY { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TBL_STATE> TBL_STATE { get; set; }
+    }
+}
