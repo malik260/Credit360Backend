f04ae89ee29f1a796c7e0ed94aac16af2b1c35bc
maurer ogbonnnaya <maurer@live.co.uk>
2018-05-18 15:32:29 +0100

    state and country fixes

--- FintrakBanking.Repositories/Setups/General/CompanyRepository.cs
+++ FintrakBanking.Repositories/Setups/General/CompanyRepository.cs
@@ -120,7 +120,7 @@ namespace FintrakBanking.Repositories.Setups.General
 
         public IEnumerable<CompanyViewModel> GetCompanies()
         {
-            var companies = (from data in context.TBL_COMPANY join cont in context.TBL_COUNTRY on data.COUNTRYID equals cont.COUNTRYID
+            var companies = (from data in context.TBL_COMPANY  
                              select new CompanyViewModel()
                              {
                                  companyId = data.COMPANYID,
@@ -148,7 +148,7 @@ namespace FintrakBanking.Repositories.Setups.General
                                  investmentObjective = data.INVESTMENTOBJECTIVE,
                                  website = data.WEBSITE,
                                  countryId = data.COUNTRYID,
-                                 country =cont.NAME ?? string.Empty,
+                                 country =  data.TBL_COUNTRY.NAME ?? string.Empty,
                                  companyClassId = data.COMPANYCLASSID ?? 1,
                                  companyTypeId = data.COMPANYTYPEID ?? 1,
                                  accountingStandardId = data.ACCOUNTINGSTANDARDID ?? 1,
@@ -281,7 +281,11 @@ namespace FintrakBanking.Repositories.Setups.General
 
         public byte[] GetCompanyLogoArray(int companyId)
         {
-            return documentContext.TBL_MEDIA_COLLATERAL_DOCUMENTS.Where(x => x.DOCUMENTID == 1).FirstOrDefault().FILEDATA;
+            return documentContext.TBL_MEDIA_COLLATERAL_DOCUMENTS
+                .FirstOrDefault(x => x.DOCUMENTID == 1)
+                ?.FILEDATA;
         }
     }
+
+    
 }
