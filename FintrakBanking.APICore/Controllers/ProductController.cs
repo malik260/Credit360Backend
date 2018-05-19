d0c299affa9c7628ef18fbe55c9c0359f6318acd
Nwaka Augustine Obinna <augustine.nwaka@fintraksoftware.com>
2018-05-19 14:44:20 +0100

    Oracle Migration update

--- FintrakBanking.APICore/Controllers/ProductController.cs
+++ FintrakBanking.APICore/Controllers/ProductController.cs
@@ -374,7 +374,30 @@ namespace FintrakBanking.APICore.Controllers
             }
         }
 
-      [HttpGet] [ClaimsAuthorization]  
+        [HttpGet]
+        [ClaimsAuthorization]
+        [Route("revolving-type")]
+        public HttpResponseMessage GetRevolvingType()
+        {
+            try
+            {
+                var data = repo.GetRevolvingTypes();
+                if (data == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK,
+                        new { success = false, message = "No record found" });
+                }
+                return Request.CreateResponse(HttpStatusCode.OK,
+                    new { success = true, result = data.ToList() });
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK,
+                    new { success = false, message = ex.Message });
+            }
+        }
+
+        [HttpGet] [ClaimsAuthorization]  
         [Route("product-type/{productTypeId}")]
         public HttpResponseMessage GetProductTypeById(short productTypeId)
         {
--- FintrakBanking.Interfaces/Setups/General/IProductRepository.cs
+++ FintrakBanking.Interfaces/Setups/General/IProductRepository.cs
@@ -10,6 +10,7 @@ namespace FintrakBanking.Interfaces.Setups.General
     public interface IProductRepository
     {
         IEnumerable<ProductCategoryViewModel> GetAllProductCategory();
+        IEnumerable<RevolvingTypeViewModel> GetRevolvingTypes();
         IEnumerable<LookupViewModel> GetProductClassByProcessId(int processId);
         IEnumerable<LookupViewModel> GetAllProductClass();
         IEnumerable<LookupViewModel> GetAllProductClass(int customerTypeId, int processId);
