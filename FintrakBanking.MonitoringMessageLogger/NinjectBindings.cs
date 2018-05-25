a0def86ac1a67c5e89341781d202fd56366b9979
isah.yarima <isah.yarima@fintraksoftware.com>
2018-05-25 19:05:17 +0100

    Email alert logger

---
+++ FintrakBanking.MonitoringMessageLogger/NinjectBindings.cs
@@ -0,0 +1,19 @@
+﻿using FintrakBanking.Interfaces.AlertMonitoring;
+using FintrakBanking.Repositories.AlertMonitoring;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.MonitoringMessageLogger
+{
+    public class NinjectBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IAlertMessageLogger>().To<AlertMessageLogger>().InSingletonScope();
+        }
+    }
+}
