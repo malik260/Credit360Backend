d0690cc27baf09c0600a1db91eb33ce2cf9bbc23
devserver <admin@admin.com>
2018-05-26 17:17:37 +0100

    show login message

--- FintrakBanking.ViewModels/Setups/General/UserViewModel.cs
+++ FintrakBanking.ViewModels/Setups/General/UserViewModel.cs
@@ -106,5 +106,8 @@ namespace FintrakBanking.ViewModels.Setups.General
         public DateTime? lastLockedOutDate { get; set; }
         public int? lastUpdatedBy { get; set; }
         public string actionMessage { get; set; }
+
+        public bool accountStatus { get; set; }
+        public bool lockStatus { get; set; }
     }
 }
