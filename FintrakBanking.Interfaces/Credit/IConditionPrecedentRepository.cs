a2442923343c070280ff3145dcf6fc4f906c7f14
developerslearnit <developerslearnit@gmail.com>
2018-06-12 11:03:59 +0100

    updates

--- FintrakBanking.Interfaces/Credit/IConditionPrecedentRepository.cs
+++ FintrakBanking.Interfaces/Credit/IConditionPrecedentRepository.cs
@@ -39,6 +39,5 @@ namespace FintrakBanking.Interfaces.Credit
         List<ConditionPrecedentViewModel> AddSelectedConditionPrecedentLms(SelectedIdsViewModel entity);
         bool AddConditionPrecedentLms(ConditionPrecedentViewModel entity);
         IEnumerable<ConditionPrecedentViewModel> GetConditionPrecedentByDetailIdLms(int detailid);
-        bool DeleteComplianceTimelineTemplate(int timelineId);
     }
 }
