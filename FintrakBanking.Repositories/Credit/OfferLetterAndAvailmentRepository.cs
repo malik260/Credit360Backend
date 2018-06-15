bb60be5f048c9ad8e0fba1de5127ceb7a916e6d2
devserver <admin@admin.com>
2018-06-15 12:43:29 +0100

    Updates

--- FintrakBanking.Repositories/Credit/OfferLetterAndAvailmentRepository.cs
+++ FintrakBanking.Repositories/Credit/OfferLetterAndAvailmentRepository.cs
@@ -1846,6 +1846,7 @@ namespace FintrakBanking.Repositories.Credit
             if (workflow.NewState == (int)ApprovalState.Ended)
             {
                 appl.APPLICATIONSTATUSID = (int)LoanApplicationStatusEnum.AvailmentInProgress;
+                PassApplicationToOperation(model.companyId, model.createdBy, (int)OperationsEnum.LoanAvailment, model.applicationId, "B&G application for availment...");
             }
 
             return context.SaveChanges() > 0;
@@ -1918,8 +1919,20 @@ namespace FintrakBanking.Repositories.Credit
 
             return data;
         }
+        
+        private void PassApplicationToOperation(int companyId, int staffId, int operationId, int targetId, string comment)
+        {
+            workflow.StaffId = staffId;
+            workflow.CompanyId = companyId;
+            workflow.OperationId = operationId;
+            workflow.TargetId = targetId;
+            workflow.ProductClassId = null;
+            workflow.StatusId = (int)ApprovalStatusEnum.Pending;
+            workflow.Comment = comment;
+            workflow.ExternalInitialization = true;
+            workflow.DeferredExecution = true;
+            workflow.LogActivity();
+        }
 
-
-        //}
     }
 }
