5bd3f3beeba3eac01d92e6bf461def601d85ee15
isah.yarima <isah.yarima@fintraksoftware.com>
2018-07-31 16:47:56 +0100

    Facility detail/Rollover/repayment

--- FintrakBanking.ViewModels/Credit/LoanViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanViewModel.cs
@@ -255,6 +255,23 @@ namespace FintrakBanking.ViewModels.Credit
         public int loadArchiveId { get; set; }
         public bool isTermLoam { get; set; }
         public bool isOD { get; set; }
+        public int postedByStaffId { get; set; }
+        public string batchNo { get; set; }
+        public decimal creditAmount { get; set; }
+        public decimal debitAmount { get; set; }
+        public string description { get; set; }
+        public DateTime valueDate { get; set; }
+        public DateTime postedDate { get; set; }
+        public DateTime postedTime { get; set; }
+        public double currencyRate { get; set; }
+        public string postCurrency { get; set; }
+        public string sourceReferenceNumber { get; set; }
+        public decimal requestedAmount { get; set; }
+        public string remark { get; set; }
+        public string nostroAccountId { get; set; }
+        public string nostroRateCode { get; set; }
+        public string notstroCurrency { get; set; }
+        public decimal? nostroRateAmount { get; set; }
 
 
         //......End f Loan Relational Table View Mapping Models......//
