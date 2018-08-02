b8afc68ec5ddf6e789c033ebc3b80863eed4e08c
devserver <admin@admin.com>
2018-08-02 07:47:00 +0100

    updates

--- FintrakBanking.Repositories/Credit/OfferLetterAndAvailmentRepository.cs
+++ FintrakBanking.Repositories/Credit/OfferLetterAndAvailmentRepository.cs
@@ -1830,11 +1830,8 @@ namespace FintrakBanking.Repositories.Credit
                 {
                     int staffId = model.createdBy;
                     int? receiverLevelId = null;
-<<<<<<< HEAD
-                    receiverLevelId = GetFirstReceiverLevel(staffId, (int)OperationsEnum.CAM, appl.PRODUCTCLASSID, true);
-=======
+
                     receiverLevelId = GetFirstReceiverLevel(staffId, (int)OperationsEnum.LoanAvailment, appl.PRODUCTCLASSID, true);
->>>>>>> e6ab9fbafca0b236b791a9faced49ed976e2c7db
 
                     workflow.StaffId = staffId;
                     workflow.NextLevelId = receiverLevelId; // BREAKING!
@@ -1874,11 +1871,6 @@ namespace FintrakBanking.Repositories.Credit
 
             var staffRoleLevels = levels.Where(x => x.staffRoleId == staff.STAFFROLEID);
             var staffRoleLevelIds = staffRoleLevels.Select(x => x.levelId);
-<<<<<<< HEAD
-            var staffRoleLevelGroupPosition = staffRoleLevels.Min(x => x.groupPosition);
-            var staffRoleLevelLevelPosition = staffRoleLevels.Min(x => x.levelPosition);
-=======
->>>>>>> e6ab9fbafca0b236b791a9faced49ed976e2c7db
             var staffRoleLevelId = staffRoleLevelIds.FirstOrDefault();
 
             if (next == false) return staffRoleLevelId;
