4f1276c33d6d2f09019939b6c7a869c3c14951e2
isah.yarima <isah.yarima@fintraksoftware.com>
2018-09-04 12:13:38 +0100

    ErrorFix

--- FintrakBanking.Entities/Models/TBL_LOAN_APPLICATION.cs
+++ FintrakBanking.Entities/Models/TBL_LOAN_APPLICATION.cs
@@ -122,7 +122,8 @@ namespace FintrakBanking.Entities.Models
         public bool SUBMITTEDFORAPPRAISAL { get; set; }
 
         public bool CUSTOMERINFOVALIDATED { get; set; }
-        public int RISKRATINGID { get; set; }
+
+        public int? RISKRATINGID { get; set; }
 
         //[Column(TypeName = "date")]
         public DateTime? APPROVEDDATE { get; set; }
@@ -191,3 +192,5 @@ namespace FintrakBanking.Entities.Models
         //public virtual ICollection<TBL_RISK_ASSESSMENT> TBL_RISK_ASSESSMENT { get; set; }
     }
 }
+
+
