f22a596c971d8c3404a8d97aadd0be67fc6e14fe
isah.yarima <isah.yarima@fintraksoftware.com>
2018-09-11 12:35:53 +0100

    OfferLetter

--- FintrakBanking.Entities/Models/TBL_LOAN_APPLICATION_DETAIL.cs
+++ FintrakBanking.Entities/Models/TBL_LOAN_APPLICATION_DETAIL.cs
@@ -113,7 +113,7 @@ namespace FintrakBanking.Entities.Models
 
         public int? CASAACCOUNTID { get; set; } //CASAACCOUNTID
 
-        public int PRODUCTPRICEINDEXID { get; set; } 
+        public int? PRODUCTPRICEINDEXID { get; set; } 
 
         public bool SECUREDBYCOLLATERAL { get; set; }
         public int? CRMSCOLLATERALTYPEID { get; set; }
--- FintrakBanking.ViewModels/Credit/LoanViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanViewModel.cs
@@ -601,6 +601,7 @@ namespace FintrakBanking.ViewModels.Credit
         public List<CamDocumentViewModel> camDocuments { get; set; }
         public short? productClassProcessId { get; set; }
         public bool undergoingConcession { get; set; }
+        public string productPriceIndex { get; set; }
         public string proposedTenorString
         {
             get
