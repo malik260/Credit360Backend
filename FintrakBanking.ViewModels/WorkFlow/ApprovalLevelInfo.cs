67ce956631b4f52a5ef9d740c65de36eef802669
Osemeke <osemekeanyirah@fintraksoftware.com>
2018-09-13 13:02:41 +0100

    refactore GetRoute Levels

---
+++ FintrakBanking.ViewModels/WorkFlow/ApprovalLevelInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.ViewModels.WorkFlow
+{
+    public class ApprovalLevelInfo
+    {
+        public int groupId { get; set; }
+        public int groupPosition { get; set; }
+        public int levelPosition { get; set; }
+        public int levelId { get; set; }
+        public string levelName { get; set; }
+        public int? staffRoleId { get; set; }
+        public int? levelTypeId { get; set; }
+    }
+}
