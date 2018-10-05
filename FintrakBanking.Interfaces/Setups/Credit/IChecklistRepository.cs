5163169a68fe2dcc66e527788b9c8c33e9cdd035
developerslearnit <developerslearnit@gmail.com>
2018-10-05 12:49:50 +0100

    checklist Simulation

--- FintrakBanking.Interfaces/Setups/Credit/IChecklistRepository.cs
+++ FintrakBanking.Interfaces/Setups/Credit/IChecklistRepository.cs
@@ -106,7 +106,6 @@ namespace FintrakBanking.Interfaces.Setups
         IEnumerable<ConditionPrecedentViewModel> GetLMSConditionPrecedenceChecklist(int loanReviewApplicationId);
         IEnumerable<ConditionPrecedentViewModel> GetLMSConditionPrecedenceChecklistStatus(int loanReviewApplicationId);
         #endregion
-
-        IEnumerable<ChecklistDefinitionAndDetailViewModel> GetChecklistItemSimulationDetails(int checkListTypeId, int operationId, int? productId, int? approvalLevelId);
+        IEnumerable<ChecklistDefinitionAndDetailViewModel> GetChecklistItemSimulationDetails(int productId);
     }
 }
