33e1a46e494620b071905f23731e1898b57f69dc
gbenga.olufemi <gbenga.olufemi@fintraksoftware.com>
2018-10-09 17:35:31 +0100

    Victor Update

--- FintrakBanking.Repositories/Credit/LoanOperationsRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanOperationsRepository.cs
@@ -1263,7 +1263,7 @@ namespace FintrakBanking.Repositories.Credit
 
 
                 FinanceTransactionStagingViewModel model = new FinanceTransactionStagingViewModel();
-                if (item.amountCollected != source.AMOUNTCOLLECTED && item.amountCollected > 0)
+                if (item.amountCollected != source.AMOUNTCOLLECTED)
                 {
                     model.actualAmount = item.amountCollected - source.AMOUNTCOLLECTED;
                     model.operationId = source.OPERATIONID;
@@ -1297,7 +1297,7 @@ namespace FintrakBanking.Repositories.Credit
                     }
 
 
-                    if (operationType == OperationsEnum.InterestLoanRepayment || operationType == OperationsEnum.PrincipalLoanRepayment)
+                    if (operationType == OperationsEnum.InterestLoanRepayment || operationType == OperationsEnum.PrincipalLoanRepayment && item.amountCollected == 0)
                     {
 
                         List<TBL_LOAN_PAST_DUE> transPastDue = new List<TBL_LOAN_PAST_DUE>();
@@ -1361,6 +1361,7 @@ namespace FintrakBanking.Repositories.Credit
                         updateloanPastDueDate((int)model.loanId, item.transactionDate);
                     }
 
+                    
                     source.AMOUNTCOLLECTED = item.amountCollected;
 
                     context.SaveChanges();
@@ -10900,6 +10901,8 @@ namespace FintrakBanking.Repositories.Credit
                                            && s.APPROVALSTATUSID != (int)ApprovalStatusEnum.Approved
                                             && s.OPERATIONCOMPLETED == false
                                             select s).FirstOrDefault();
+
+
 
                         if (entity.approvalStatusId == (short)ApprovalStatusEnum.Disapproved)
                         {
