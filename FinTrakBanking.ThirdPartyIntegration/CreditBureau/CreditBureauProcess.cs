d93fabfbcc6a9ffc5bc729d0829308bccdaf8115
Nwaka Augustine Obinna <augustine.nwaka@fintraksoftware.com>
2018-10-13 15:27:52 +0100

    Job Request on offer letter review

--- FinTrakBanking.ThirdPartyIntegration/CreditBureau/CreditBureauProcess.cs
+++ FinTrakBanking.ThirdPartyIntegration/CreditBureau/CreditBureauProcess.cs
@@ -260,8 +260,6 @@
             }
 
             private string DoXDSIndividualSearch(CreditBureauSearchViewModel searchInfo)
-
-
             {
                 string result = string.Empty;
                 XDSService xds = new XDSService();
