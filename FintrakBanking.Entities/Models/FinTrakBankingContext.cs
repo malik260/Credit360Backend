5ef0fd99a3f0842a8a7cce684d4eb31263844aff
gbenga.olufemi <gbenga.olufemi@fintraksoftware.com>
2018-11-13 18:12:57 +0100

    Victor Update

--- FintrakBanking.Entities/Models/FinTrakBankingContext.cs
+++ FintrakBanking.Entities/Models/FinTrakBankingContext.cs
@@ -14,6 +14,7 @@ namespace FintrakBanking.Entities.Models
         }
 
         //public virtual DbSet<ELMAH_ERROR> ELMAH_ERROR { get; set; }
+        //public virtual DbSet<TBL_ENDOFDAY_MONITORING> TBL_ENDOFDAY_MONITORING { get; set; }
         public virtual DbSet<TBL_ACCOUNT_CATEGORY> TBL_ACCOUNT_CATEGORY { get; set; }
         public virtual DbSet<TBL_ACCOUNT_TYPE> TBL_ACCOUNT_TYPE { get; set; }
         public virtual DbSet<TBL_ACCOUNTING_STANDARD> TBL_ACCOUNTING_STANDARD { get; set; } 
