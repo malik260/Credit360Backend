88db989e3baf38837fc5df73a6c3775ae2ed4267
devserver <admin@admin.com>
2018-11-21 13:39:08 +0100

    update

---
+++ FintrakBanking.Common/Enum/RequiredCollateralTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FintrakBanking.Common.Enum
+{
+    public enum RequiredCollateralTypeEnum
+    {
+        ImmovablePropertyCollateral = 1,
+        OtherCollateral = 2,
+        NoCollateral = 3,
+    }
+}
