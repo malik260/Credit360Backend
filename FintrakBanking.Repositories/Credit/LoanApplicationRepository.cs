6d0ad9fbf00f7fdde56950754078bc60a497f81d
isah.yarima <isah.yarima@fintraksoftware.com>
2018-11-21 17:41:35 -0800

    offer letter

--- FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
@@ -692,7 +692,7 @@ namespace FintrakBanking.Repositories.Credit
                            applicationTenor = Math.Round((double)a.APPLICATIONTENOR) * (12.0 / 365.0),
                            applicationAmount = a.APPLICATIONAMOUNT,
                            regionId = a.CAPREGIONID,
-                           requiredCollateralTypeId = a.REQUIREDCOLLATERALTYPEID,
+                           requiredCollateralTypeId = a.REQUIRECOLLATERALTYPEID,
                            preliminaryEvaluationId = a.LOANPRELIMINARYEVALUATIONID,
                            collateralDetail = a.COLLATERALDETAIL,
                            loanInformation = a.LOANINFORMATION,
@@ -1353,7 +1353,7 @@ namespace FintrakBanking.Repositories.Credit
             this.loanData.APPLICATIONTENOR = application.Max(c => c.PROPOSEDTENOR);
             this.loanData.COLLATERALDETAIL = loan.collateralDetail;
             this.loanData.CAPREGIONID = loan.regionId;
-            this.loanData.REQUIREDCOLLATERALTYPEID = loan.requiredCollateralTypeId;
+            this.loanData.REQUIRECOLLATERALTYPEID = loan.requiredCollateralTypeId;
         }
 
         private void TradderLoan(TraderLoanViewModel entity, int loanApplicationId, int createdBy)
