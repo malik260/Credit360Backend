8904907eaa8514ccb32697c10011dd49242182ea
Nwaka Augustine Obinna <augustine.nwaka@fintraksoftware.com>
2018-11-24 16:46:43 +0100

    Job Sub Type Class Inclussion

---
+++ FintrakBanking.Common/Enum/JobRequestEnum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.Common.Enum
+{
+    public enum JobTypeEnum
+    {
+        legal = 1,
+        middleOfficeVerification  = 2,
+        camsolCheck = 4,
+        blackBookCheck   = 5,
+        others = 6
+
+    }
+
+    public enum JobSubTypeEnum
+    {
+        CollateralRelated = 1,
+        BondsAndGauranteeVetting = 2,
+        Others = 3,
+        MiddleOfficeVerification = 4,
+        CAMSOLCheck = 5,
+        BlackBookCheck = 6,
+        ConfirmationOfTreasuryBills = 7,
+        ConfirmationOfDealSlip = 8,
+        ConfirmationOfStock = 9,
+        ConfirmationOfFBNQUEST = 10,
+    }
+
+
+    public enum JobRequestStatusEnum
+    {
+        approved = 3,
+        cancel = 5,
+        disapproved = 4,
+        pending = 1,
+        processing = 2,
+
+    }
+
+    public enum JobSubTypeClassEnum
+    {
+        CollateralSearch = 1,
+        CollateralVerification = 2,
+        CollateralCharting = 3,
+        AdditionalCharges =  4,
+    }
+
+
+}
