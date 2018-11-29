9ffaab93f0b12668b224f8dda891dac8f9420122
developerslearnit <developerslearnit@gmail.com>
2018-11-29 19:45:01 +0100

    Fixed Logging Status report issues

--- FintrakBanking.APICore/Controllers/ReportsController.cs
+++ FintrakBanking.APICore/Controllers/ReportsController.cs
@@ -1625,6 +1625,32 @@ namespace FintrakBanking.APICore.Controllers
             }
         }
 
+
+        [HttpPost]
+        [ClaimsAuthorization]
+        [Route("login-status")]
+        public HttpResponseMessage GetLoggingStatus(DateRange param)
+        {
+            var token = new TokenDecryptionHelper();
+            try
+            {
+
+                param.companyId = token.GetCompanyId;
+                var data = repo.GetLoggingStatus(param);
+                if (data == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK,
+                        new { success = false, message = "No record found" });
+                }
+                return Request.CreateResponse(HttpStatusCode.OK,
+                    new { success = true, result = data });  //Ok(accounts);
+            }
+            catch (SecureException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = ex.Message });
+            }
+        }
+
     }
 }
 
