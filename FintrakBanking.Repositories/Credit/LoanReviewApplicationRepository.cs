42ece58b6b10c98bfaaaad2223f7f0991b0ae275
Osemeke <osemekeanyirah@fintraksoftware.com>
2019-01-10 10:58:19 +0100

    update lms page 4

--- FintrakBanking.Repositories/Credit/LoanReviewApplicationRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanReviewApplicationRepository.cs
@@ -322,7 +322,6 @@ namespace FintrakBanking.Repositories.Credit
 
             foreach (var detail in model.applicationDetails)
             {
-                customerIds.Add(loan.customerId);
                 loan = GetLoanInformation(detail.loanSystemTypeId, detail.loanId, applicationDate);
                 int tenor = detail.loanSystemTypeId == 4 ? loan.tenorUsed : loan.tenor;
 
@@ -352,6 +351,8 @@ namespace FintrakBanking.Repositories.Credit
 
                     //LOANAPPLICATIONDETAILID = loan.loanApplicationDetailId,
                 });
+
+                customerIds.Add(loan.customerId);
             }
 
             // ------------AUDIT CODE HERE! -------------
