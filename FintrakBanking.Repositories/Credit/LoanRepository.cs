5d210231b290c60309d3b1a8975786fb46ef736e
isah.yarima <isah.yarima@fintraksoftware.com>
2019-01-21 21:06:31 -0800

    error fix

--- FintrakBanking.Repositories/Credit/LoanRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanRepository.cs
@@ -2362,7 +2362,7 @@ namespace FintrakBanking.Repositories.Credit
                                 branchId = ln.BRANCHID,
                                 loanReferenceNumber = ln.LOANREFERENCENUMBER,
                                 applicationReferenceNumber = ln.TBL_LOAN_APPLICATION_DETAIL.TBL_LOAN_APPLICATION.APPLICATIONREFERENCENUMBER,
-
+                                crmsCode = ln.CRMSCODE,
                                 staffId = staffId,
 
                                 pricipalFrequencyTypeName = ln.TBL_FREQUENCY_TYPE.DESCRIPTION ?? null,
@@ -8976,7 +8976,7 @@ namespace FintrakBanking.Repositories.Credit
                                        || a.LOANSTATUSID != (short)LoanStatusEnum.Inactive
                                        || a.LOANSTATUSID != (short)LoanStatusEnum.Completed
                                    ) 
-                                   && a.MATURITYDATE > applicationDate
+                                  // && a.MATURITYDATE > applicationDate
                                    select new LoanViewModel
                                    {
                                        loanId = a.TERMLOANID,
@@ -9023,7 +9023,7 @@ namespace FintrakBanking.Repositories.Credit
                                    b.LASTNAME.ToLower().Contains(searchQuery.Trim()) ||
                                    c.PRODUCTACCOUNTNUMBER.ToLower().Contains(searchQuery.Trim())) 
                                    && (a.LOANSTATUSID != (short)LoanStatusEnum.Cancelled || a.LOANSTATUSID != (short)LoanStatusEnum.Terminated)  //|| a.LOANSTATUSID != (short)LoanStatusEnum.Inactive || a.LOANSTATUSID != (short)LoanStatusEnum.Completed
-                                   && a.MATURITYDATE > applicationDate
+                                  // && a.MATURITYDATE > applicationDate
                                    select new LoanViewModel
                                    {
                                        loanId = a.REVOLVINGLOANID,
