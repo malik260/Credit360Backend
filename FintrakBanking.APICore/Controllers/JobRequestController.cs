8edd7d83eb4cd95198049540739169e893721848
Nwaka Augustine Obinna <augustine.nwaka@fintraksoftware.com>
2019-01-26 12:46:43 +0100

    credit bureau charges

--- FintrakBanking.APICore/Controllers/JobRequestController.cs
+++ FintrakBanking.APICore/Controllers/JobRequestController.cs
@@ -297,6 +297,7 @@ namespace FintrakBanking.APICore.Controllers
                 entity.createdBy = token.GetStaffId;
                 entity.userBranchId = (short)token.GetBranchId;
                 entity.applicationUrl = HttpContext.Current.Request.Path;
+                entity.branchId = (short)token.GetBranchId;
 
                 var code = repo.AddGlobalJobRequest(entity);
                 if (code != null)
@@ -570,6 +571,7 @@ namespace FintrakBanking.APICore.Controllers
                 entity.applicationUrl = HttpContext.Current.Request.Path;
 
                 requestModel.userBranchId = (short)token.GetBranchId;
+                requestModel.branchId = (short)token.GetBranchId;
                 requestModel.companyId = token.GetCompanyId;
                 requestModel.createdBy = token.GetStaffId;
                 requestModel.applicationUrl = HttpContext.Current.Request.Path;
