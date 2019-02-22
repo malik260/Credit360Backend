cdb89050133710335ab8b0815927a3fc760b1c96
isah.yarima <isah.yarima@yahoo.com>
2019-02-22 13:02:03 -0800

    update

---
+++ FintrakBanking.BackgroundTasksService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FintrakBanking.BackgroundTasksService.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FintrakBanking.BackgroundTasksService/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Oracle.Core;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FintrakBanking.BackgroundTasksService.Startup))]
+
+namespace FintrakBanking.BackgroundTasksService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["FinTrakBankingContext"].ToString();
+            GlobalConfiguration.Configuration.UseStorage(
+                new OracleStorage(
+                    connectionString,
+                    new OracleStorageOptions
+                    {
+                        TransactionIsolationLevel = IsolationLevel.ReadCommitted,
+                        QueuePollInterval = TimeSpan.FromSeconds(15),
+                        JobExpirationCheckInterval = TimeSpan.FromHours(1),
+                        CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                        PrepareSchemaIfNecessary = false,
+                        DashboardJobListLimit = 50000,
+                        TransactionTimeout = TimeSpan.FromMinutes(1),
+                        SchemaName = "HANGFIRE"
+                    }));
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+        }
+    }
+}
