e3c3a90f3481d7ad48846989390903afdbe9d9c5
devserver <admin@admin.com>
2019-02-28 10:48:42 +0100

    update

--- FintrakBanking.APICore/Controllers/CustomerCollateralController.cs
+++ FintrakBanking.APICore/Controllers/CustomerCollateralController.cs
@@ -1096,9 +1096,13 @@ namespace FintrakBanking.APICore.Controllers
                 var response = type.CollateralSubType(id);
                 return Request.CreateResponse(HttpStatusCode.OK, new { success = true, result = response });
             }
-            catch (SecureException ex)
+            //catch (SecureException ex)
+            //{
+            //    return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = ex.Message });
+            //}
+            catch (ConditionNotMetException ce)
             {
-                return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = $"Error: {ce.Message}" });
             }
         }
 
