9607ca059f792a5959ae7beccd45aa92f54b5598
isah.yarima <isah.yarima@yahoo.com>
2019-03-01 17:17:15 -0800

    form3800bReport

--- FintrakBanking.ViewModels/Credit/LoanViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanViewModel.cs
@@ -1504,6 +1504,7 @@ namespace FintrakBanking.ViewModels.Credit
         public string subHead { get; set; }
         public string staffCode { get; set; }
         public string teamUnit { get; set; }
+        public string depart { get; set; }
       
     }
     //public class MaturityIntructionViewModel : GeneralEntity
