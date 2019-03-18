648010ce2c1788be39eff467e2c4f46af941b9e1
devserver <admin@admin.com>
2019-03-18 17:39:57 +0100

    updates

--- FinTrakBanking.ThirdPartyIntegration/Finacle/CWGAPI/TransactionPosting.cs
+++ FinTrakBanking.ThirdPartyIntegration/Finacle/CWGAPI/TransactionPosting.cs
@@ -361,7 +361,7 @@
                     handler.UseDefaultCredentials = true;
                     httpClientInstance = new HttpClient();
                     httpClientInstance.DefaultRequestHeaders.ConnectionClose = false;
-                    client.Timeout = TimeSpan.FromSeconds(60);
+                    client.Timeout = TimeSpan.FromSeconds(180);
                     client.DefaultRequestHeaders.Authorization = token;
                     client.BaseAddress = new Uri(API_URL);
                     client.DefaultRequestHeaders.Accept.Clear();
