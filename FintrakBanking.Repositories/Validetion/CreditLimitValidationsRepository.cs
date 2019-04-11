724fedb589e36e93333e5cc383088b3d42dd9d31
devserver <anyirahosemeke@gmail.com>
2019-04-11 18:10:12 +0100

    update

--- FintrakBanking.Repositories/Validetion/CreditLimitValidationsRepository.cs
+++ FintrakBanking.Repositories/Validetion/CreditLimitValidationsRepository.cs
@@ -628,14 +628,14 @@ namespace FintrakBanking.Repositories.CreditLimitValidations
             var initiated = (from a in context.TBL_LOAN_APPLICATION
                              join d in context.TBL_LOAN_APPLICATION_DETAIL on a.LOANAPPLICATIONID equals d.LOANAPPLICATIONID
                              where d.STATUSID == (short)ApprovalStatusEnum.Approved && a.APPROVEDDATE == null
-                             select d.APPROVEDAMOUNT).Sum();
+                             select (decimal?)d.APPROVEDAMOUNT).Sum() ?? 0;
 
             var approved = (from a in context.TBL_LOAN_APPLICATION
                             join d in context.TBL_LOAN_APPLICATION_DETAIL on a.LOANAPPLICATIONID equals d.LOANAPPLICATIONID
                             where d.STATUSID == (short)ApprovalStatusEnum.Approved && a.APPROVEDDATE != null &&
                             (a.APPLICATIONSTATUSID != (int)LoanApplicationStatusEnum.BookingRequestCompleted &&
                             a.APPLICATIONSTATUSID != (int)LoanApplicationStatusEnum.BookingRequestInitiated)
-                            select d.APPROVEDAMOUNT).Sum();
+                            select (decimal?)d.APPROVEDAMOUNT).Sum() ?? 0;
 
             return new CreditLimitValidationsModel
             {
