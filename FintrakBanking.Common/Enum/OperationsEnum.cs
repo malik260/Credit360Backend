05439eddc88e4f033e9d2561d18e689856db9c97
devserver <anyirahosemeke@gmail.com>
2019-07-18 09:58:42 +0100

    update

--- FintrakBanking.Common/Enum/OperationsEnum.cs
+++ FintrakBanking.Common/Enum/OperationsEnum.cs
@@ -132,8 +132,6 @@ namespace FintrakBanking.Common.Enum
         lcUssance = 140,
         CreditCardDrawdownRequest = 141,
         LetterGenerationRequest = 142,
-        CollateralValuationRequest = 143,
-        CollateralValuationApproval = 144,
         //CreditCardsSalaryBacked = 145,
         CollateralValuationRequest = 144,
         CreditCardsSalaryBacked = 145,
