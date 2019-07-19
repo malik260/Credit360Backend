abc5ce086fa5b0ca00167fed67a3d10165474ee8
devserver <anyirahosemeke@gmail.com>
2019-07-19 09:27:12 +0100

    update

---
+++ FintrakBanking.Entities/Models/TBL_RAC_CATEGORY_TYPE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.Entities.Models
+{
+    [Table("TBL_RAC_CATEGORY_TYPE")]
+    public class TBL_RAC_CATEGORY_TYPE
+    {
+        [Key]
+        public int RACCATEGORYTYPEID { get; set; }
+        public int RACCATEGORYID { get; set; }
+        public string RACCATEGORYTYPE { get; set; }
+    }
+}
