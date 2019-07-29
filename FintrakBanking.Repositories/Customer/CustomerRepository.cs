5c12ec101a3fa16845cb5165f76024aa32088a97
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2019-07-29 17:10:38 +0100

    Appraisal customerTotalExposure

--- FintrakBanking.Repositories/Customer/CustomerRepository.cs
+++ FintrakBanking.Repositories/Customer/CustomerRepository.cs
@@ -3212,8 +3212,8 @@ namespace FintrakBanking.Repositories.Customer
                             riskRatingName = a.TBL_CUSTOMER_RISK_RATING.RISKRATING,
                             customerBVN = a.CUSTOMERBVN,
                         }).FirstOrDefault();
-            // if (USE_THIRD_PARTY_INTEGRATION)
-            //    data.isPoliticallyExposed = finacle.GetExposePersonStatus(data.customerCode);
+            if (USE_THIRD_PARTY_INTEGRATION)
+                data.isPoliticallyExposed = finacle.GetExposePersonStatus(data.customerCode);
 
             return data;
         }
