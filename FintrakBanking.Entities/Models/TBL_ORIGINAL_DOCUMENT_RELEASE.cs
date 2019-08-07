0f81af54931f39a0361a30f28ce5d44a345d95de
adesola.abiola <adesola.abiola@fintraksoftware.com>
2019-08-07 11:35:49 +0100

    ATCUpdates

--- FintrakBanking.Entities/Models/TBL_ORIGINAL_DOCUMENT_RELEASE.cs
+++ FintrakBanking.Entities/Models/TBL_ORIGINAL_DOCUMENT_RELEASE.cs
@@ -23,7 +23,7 @@ namespace FintrakBanking.Entities.Models
         public int CREATEDBY { get; set; }
         public DateTime? DATETIMECREATED { get; set; }
         public int LASTUPDATEDBY { get; set; }
-        public DateTime DATETIMEUPDATED { get; set; }
+        public DateTime? DATETIMEUPDATED { get; set; }
         public bool DELETED { get; set; }
         public int DELETEDBY { get; set; }
         public DateTime DATETIMEDELETED { get; set; }
