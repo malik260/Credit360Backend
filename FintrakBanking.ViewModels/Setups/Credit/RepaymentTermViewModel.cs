4a17535fddd35f263fb1c4646836c306dcff9e82
ifeanyi.ikefuna <ikefunastephen@gmail.com>
2019-08-09 13:32:53 +0100

    after2

---
+++ FintrakBanking.ViewModels/Setups/Credit/RepaymentTermViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.ViewModels.Setups.Credit
+{
+    class RepaymentTermViewModel : GeneralEntity
+    {
+        public int repaymentTermId { get; set; }
+
+        public string RepaymentTermIdDetail { get; set; }
+
+        public bool DELETED { get; set; }
+    }
+}
