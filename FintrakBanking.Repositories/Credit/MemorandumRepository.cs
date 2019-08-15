f187ca4321006e6424ece5f5fdde42f00c950287
devserver <anyirahosemeke@gmail.com>
2019-08-15 19:04:15 +0100

    fam

--- FintrakBanking.Repositories/Credit/MemorandumRepository.cs
+++ FintrakBanking.Repositories/Credit/MemorandumRepository.cs
@@ -266,7 +266,6 @@ namespace FintrakBanking.Repositories.Credit
 
             this.targetId = targetId;
             this.operationId = operationId;
-            this.customerFacilities = context.TBL_LOAN_APPLICATION_DETAIL.Where(f => f.DELETED == false && f.CUSTOMERID == this.customerId).ToList();
             if (operationId == (int)OperationsEnum.CreditAppraisal) // LOS 
             {
                 if (loanApplication == null)
@@ -288,6 +287,7 @@ namespace FintrakBanking.Repositories.Credit
                     this.customerId = (int)loanApplication.CUSTOMERID;
                 }
 
+                this.customerFacilities = context.TBL_LOAN_APPLICATION_DETAIL.Where(f => f.DELETED == false && f.CUSTOMERID == this.customerId).ToList();
                 this.branchName = loanApplication.TBL_BRANCH.BRANCHNAME;
                 this.locationName = loanApplication.TBL_BRANCH.ADDRESSLINE1 + " " + loanApplication.TBL_BRANCH.ADDRESSLINE2;
                 this.isRelatedParty = loanApplication.ISRELATEDPARTY == true ? "Yes" : "No";
