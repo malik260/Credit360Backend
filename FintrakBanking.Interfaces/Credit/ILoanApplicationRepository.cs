034bf7ff6a63c2fe6c00074eb632c34913cb3a94
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2019-08-22 11:04:07 +0100

    commiting the latest copy of the back-end solution by Benjamin

--- FintrakBanking.Interfaces/Credit/ILoanApplicationRepository.cs
+++ FintrakBanking.Interfaces/Credit/ILoanApplicationRepository.cs
@@ -161,5 +161,6 @@ namespace FintrakBanking.Interfaces.Credit
 
         bool DeleteLoanApplicationFlowChange(int id, UserInfo user);
         List<RatingAndRatioViewModel> GetCustomerRatios(int customerId, int applicationId, bool isLms = false);
+        CustomerApplicationTransactionsViewModels GetCustomerTransactionsByFilterLogic(int customerId, int applicationId, int froma, int to, int fYear, int tYear, bool v);
     }
 }
