0db24f3b59cd4d6c64024ae641617d5bd37eba13
Kingsley Ozoemena <kingsley.ozoemena@fintraksoftware.com>
2019-08-23 18:09:10 +0100

    workflowchange

--- FintrakBanking.APICore/Controllers/LoanApplicationController.cs
+++ FintrakBanking.APICore/Controllers/LoanApplicationController.cs
@@ -1707,6 +1707,16 @@ namespace FintrakBanking.APICore.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, new { success = true, result = response, count = 1 });
         }
 
+        [HttpDelete]
+        [ClaimsAuthorization]
+        [Route("delete-failedrac-loan-application/{loanApplicationId}")]
+        public HttpResponseMessage DeleteLoanApplicationThatFailedRAC(int loanApplicationId)
+        {
+            var response = repo.DeleteLoanApplicationThatFailedRAC(loanApplicationId, token.GetStaffId);
+            if (!response) return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = "No record found" });
+            return Request.CreateResponse(HttpStatusCode.OK, new { success = true, result = response, count = 1 });
+        }
+
 
         [HttpGet]
         [ClaimsAuthorization]
@@ -1735,14 +1745,67 @@ namespace FintrakBanking.APICore.Controllers
             
         }
 
-        [HttpDelete]
+        [HttpGet]
         [ClaimsAuthorization]
-        [Route("delete-failedrac-loan-application/{loanApplicationId}")]
-        public HttpResponseMessage DeleteLoanApplicationThatFailedRAC(int loanApplicationId)
+        [Route("loan-application-flow-change/{id}")]
+        public HttpResponseMessage GetLoanApplicationFlowChange(int id)
         {
-            var response = repo.DeleteLoanApplicationThatFailedRAC(loanApplicationId, token.GetStaffId);
-            if (!response) return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = "No record found" });
+            LoanApplicationFlowChangeViewModel response =repo.GetLoanAppicationFlowChange(id);
+            if (response == null) return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = "No record found" });
             return Request.CreateResponse(HttpStatusCode.OK, new { success = true, result = response, count = 1 });
         }
+
+       
+
+
+        [HttpPost]
+        [ClaimsAuthorization]
+        [Route("loan-application-flow-change")]
+        public HttpResponseMessage AddLoanApplicationFlowChange([FromBody] LoanApplicationFlowChangeViewModel model)
+        {
+            model.userBranchId = (short)token.GetBranchId;
+            model.userIPAddress = HttpContext.Current.Request.UserHostAddress;
+            model.applicationUrl = HttpContext.Current.Request.Path;
+            model.createdBy = token.GetStaffId;
+            model.companyId = token.GetCompanyId;
+            var response = repo.AddLoanApplicationFlowChange(model);
+            if (response) return Request.CreateResponse(HttpStatusCode.OK, new { success = true, result = response, message = "The record has been created successfully" });
+            return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = "There was an error creating this record" });
+        }
+
+        [HttpPut]
+        [ClaimsAuthorization]
+        [Route("loan-application-flow-change/{id}")]
+        public HttpResponseMessage UpdateLoanApplicationFlowChange([FromBody] LoanApplicationFlowChangeViewModel model, int id)
+        {
+            UserInfo user = new UserInfo()
+            {
+                BranchId = token.GetBranchId,
+                companyId = token.GetCompanyId,
+                createdBy = token.GetStaffId,
+                applicationUrl = HttpContext.Current.Request.Path,
+                userIPAddress = HttpContext.Current.Request.UserHostAddress
+            };
+            bool response = repo.UpdateLoanApplicationFlowChange(model, id, user);
+            return Request.CreateResponse(HttpStatusCode.OK, new { success = response, result = response, count = 1 });
+        }
+
+        [HttpDelete]
+        [ClaimsAuthorization]
+        [Route("loan-application-flow-change/{id}")]
+        public HttpResponseMessage DeleteLoanApplicationFlowChange(int id)
+        {
+            UserInfo user = new UserInfo()
+            {
+                BranchId = token.GetBranchId,
+                companyId = token.GetCompanyId,
+                createdBy = token.GetStaffId,
+                applicationUrl = HttpContext.Current.Request.Path,
+                userIPAddress = HttpContext.Current.Request.UserHostAddress
+            };
+            bool response = repo.DeleteLoanApplicationFlowChange(id, user);
+            return Request.CreateResponse(HttpStatusCode.OK, new { success = response, result = response, count = 1 });
+        }
+
     }
 }
--- FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
@@ -4722,6 +4722,7 @@ namespace FintrakBanking.Repositories.Credit
                 operationId = entity.OPERATIONID,
                 destinationUrl = entity.DESTINATIONURL,
                 productTypeId = entity.PRODUCTTYPEID,
+                documentOperation=entity.DOCUMENTOPERATION,
             };
         }
 
@@ -4731,6 +4732,7 @@ namespace FintrakBanking.Repositories.Credit
                 .Select(x => new LoanApplicationFlowChangeViewModel
                 {
                     FlowChangeId = x.FLOWCHANGEID,
+                    documentOperation=x.DOCUMENTOPERATION,
                     label = x.LABEL,
                     placeHolder=x.PLACEHOLDER,
                     operationId=x.OPERATIONID,
@@ -4750,6 +4752,7 @@ namespace FintrakBanking.Repositories.Credit
         {
             var entity = new TBL_LOAN_APPLICATN_FLOW_CHANGE
             {
+                DOCUMENTOPERATION = model.documentOperation,
                 LABEL = model.label,
                 PLACEHOLDER = model.placeHolder,
                 ISSKIPPROCESSENABLED = model.skipflow,
@@ -4757,7 +4760,10 @@ namespace FintrakBanking.Repositories.Credit
                 OPERATIONID = model.operationId,
                 DESTINATIONURL = model.destinationUrl,
                 PRODUCTTYPEID = model.productTypeId,
-                
+                DATETIMECREATED = DateTime.Now,
+                CREATEDBY = model.createdBy,
+                DELETED = false
+
             };
             context.TBL_LOAN_APPLICATN_FLOW_CHANGE.Add(entity);
 
@@ -4788,6 +4794,7 @@ namespace FintrakBanking.Repositories.Credit
             entity.PRODUCTCLASSID = model.productClassId;
             entity.OPERATIONID = model.operationId;
             entity.DESTINATIONURL = model.destinationUrl;
+            entity.DOCUMENTOPERATION = model.documentOperation;
             entity.PRODUCTTYPEID = model.productTypeId;
             var auditStaff = (context.TBL_STAFF.Where(x => x.STAFFID == user.createdBy).Select(x => x.STAFFCODE));
             // Audit Section ---------------------------
