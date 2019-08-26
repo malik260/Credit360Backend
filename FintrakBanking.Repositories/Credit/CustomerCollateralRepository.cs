30afff2af666281af99dc0adfb98b90e547134e2
adesola.abiola <adesola.abiola@fintraksoftware.com>
2019-08-26 17:05:12 +0100

    Insurance

--- FintrakBanking.Repositories/Credit/CustomerCollateralRepository.cs
+++ FintrakBanking.Repositories/Credit/CustomerCollateralRepository.cs
@@ -1319,9 +1319,9 @@ namespace FintrakBanking.Repositories.Credit
         public bool AddInsurancePolicy(InsurancePolicies entity)
         {
             var result = context.TBL_COLLATERAL_ITEM_POLICY.Where(ip => ip.COLLATERALCUSTOMERID == entity.collateraalId
-                                                                    && (ip.APPROVALSTATUSID != (short)ApprovalStatusEnum.Approved 
-                                                                    || ip.APPROVALSTATUSID != (short)ApprovalStatusEnum.Disapproved )).Any();
-            if (result) return false;
+                                                                    && ip.APPROVALSTATUSID != (short)ApprovalStatusEnum.Approved
+                                                                    && ip.APPROVALSTATUSID != (short)ApprovalStatusEnum.Disapproved).ToList();
+            if (result.Count > 0) return false;
 
             var policy = context.TBL_COLLATERAL_ITEM_POLICY.Add(new TBL_COLLATERAL_ITEM_POLICY
             {
