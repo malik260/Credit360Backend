d1ff23b3c6bf817da83efaa26f0566145d456729
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2019-09-06 07:53:41 +0100

    commiting the latest copy of the back-end solution by Benjamin

--- FintrakBanking.Repositories/Setups/Credit/BulkDisbursementPackageRepository.cs
+++ FintrakBanking.Repositories/Setups/Credit/BulkDisbursementPackageRepository.cs
@@ -329,6 +329,9 @@ namespace FintrakBanking.Repositories.Setups.Credit
             var search = searchString.Trim().ToLower();
 
             var applications = (from a in context.TBL_LOAN_BULK_DISBURSE_SCHEME
+                                join x in context.TBL_LOAN_APPLICATION_DETAIL on a.LOANAPPLICATIONDETAILID equals x.LOANAPPLICATIONDETAILID
+                                join b in context.TBL_LOAN_APPLICATION on x.LOANAPPLICATIONID equals b.LOANAPPLICATIONID
+                                join c in context.TBL_CUSTOMER on x.CUSTOMERID equals c.CUSTOMERID
                                 where (a.SCHEMECODE.Contains(search)
                                       || a.SCHEMENAME.ToLower().Contains(search))
                                 select new BulkDisbursementSetupSchemeViewModel
@@ -339,6 +342,9 @@ namespace FintrakBanking.Repositories.Setups.Credit
                                     principalFrequencyTypeId = a.PRINCIPALFREQUENCYTYPEID,
                                     loanApplicationDetailId = a.LOANAPPLICATIONDETAILID,
                                     schemeCode = a.SCHEMECODE,
+                                    customerName = c.LASTNAME + " " + c.FIRSTNAME + " " + c.MIDDLENAME,
+                                    applicationReferenceNumber = b.APPLICATIONREFERENCENUMBER,
+                                    loanApplicationId = b.LOANAPPLICATIONID,
                                     productPriceIndexId = (int)a.PRODUCTPRICEINDEXID,
                                     scheduleMethodId = (int)a.SCHEDULEMETHODID,
                                     productId = a.PRODUCTID,
