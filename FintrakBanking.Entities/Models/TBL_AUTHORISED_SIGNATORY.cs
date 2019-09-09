a7443c35264a606d393e7346bd4756c1572aa95f
ifeanyi.ikefuna <ikefunastephen@gmail.com>
2019-09-09 18:38:29 +0100

    B4PULL

--- FintrakBanking.Entities/Models/TBL_AUTHORISED_SIGNATORY.cs
+++ FintrakBanking.Entities/Models/TBL_AUTHORISED_SIGNATORY.cs
@@ -14,5 +14,12 @@
         public string SIGNATORYNAME { get; set; }
         public string SIGNATORYTITLE { get; set; }
         public string SIGNATORYINITIALS { get; set; }
+        public bool DELETED { get; set; }
+        public int CREATEDBY { get; set; }
+        public DateTime DATETIMECREATED { get; set; }
+        public int? LASTUPDATEDBY { get; set; }
+        public DateTime? DATETIMEUPDATED { get; set; }
+        public int? DELETEDBY { get; set; }
+        public DateTime? DATETIMEDELETED { get; set; }
     }
 }
