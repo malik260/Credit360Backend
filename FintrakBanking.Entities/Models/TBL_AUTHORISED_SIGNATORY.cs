38bd959f413215cc907278b5064bbcf7d6797b49
ifeanyi.ikefuna <ikefunastephen@gmail.com>
2019-09-09 18:36:45 +0100

    B4PULL

---
+++ FintrakBanking.Entities/Models/TBL_AUTHORISED_SIGNATORY.cs
@@ -0,0 +1,18 @@
+﻿namespace FintrakBanking.Entities.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class TBL_AUTHORISED_SIGNATORY
+    {
+        [Key]
+        public int SIGNATORYID { get; set; }
+
+        public string SIGNATORYNAME { get; set; }
+        public string SIGNATORYTITLE { get; set; }
+        public string SIGNATORYINITIALS { get; set; }
+    }
+}
