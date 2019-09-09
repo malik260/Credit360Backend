37c933c6791b07278d461fef1566c9cf1dc08f1e
Kingsley Ozoemena <kingsley.ozoemena@fintraksoftware.com>
2019-09-09 19:19:14 +0100

    update

---
+++ FintrakBanking.Interfaces/Setups/General/IAuthourisedSignatoriesRepository.cs
@@ -0,0 +1,19 @@
+﻿using FintrakBanking.ViewModels.Setups.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.Interfaces.Setups.General
+{
+   public interface IAuthourisedSignatoriesRepository
+    {
+     
+        AuthourisedSignatoriesViewModel SignatoryName(int id);
+        IEnumerable<AuthourisedSignatoriesViewModel> GetSignatories();
+        bool AddSignatory(AuthourisedSignatoriesViewModel model);
+        bool DeleteSignatory(AuthourisedSignatoriesViewModel model);
+   
+    }
+}
---
+++ FintrakBanking.ViewModels/Setups/General/AuthourisedSignatoriesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.ViewModels.Setups.General
+{
+    public  class AuthourisedSignatoriesViewModel
+    {
+        public int signatoryId { get; set; }
+        public string signatoryName { get; set; }
+        public string signatoryLevel { get; set; }
+        public string signatoryInitial { get; set; }
+        
+    }
+}
