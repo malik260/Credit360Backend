4a7c13a39d625fb363c7dd4658bdbd43ec6352dc
Adeyemi Olaopa <adeyemi.olaopa@fintraksoftware.com>
2019-09-16 17:45:00 +0100

    Collateral Valuation

--- FintrakBanking.Repositories/Credit/CollateralValuationRepository.cs
+++ FintrakBanking.Repositories/Credit/CollateralValuationRepository.cs
@@ -391,13 +391,11 @@ namespace FintrakBanking.Repositories.Credit
         
             try
             {
-                //foreach (var valuation in valuations)
-                //{
-                prerequisite.APPROVALSTATUSID = (int) ApprovalStatusEnum.Processing;
-                //}
 
                 if (prerequisite != null)
                 {
+                    prerequisite.APPROVALSTATUSID = (int)ApprovalStatusEnum.Processing;
+
                     _workflow.StaffId = entity.createdBy;
                     _workflow.CompanyId = entity.companyId;
                     _workflow.StatusId = (int) ApprovalStatusEnum.Processing;
@@ -591,5 +589,37 @@ namespace FintrakBanking.Repositories.Credit
 
             return false;
         }
+
+        public bool UpdateValuationPrerequisiteStatus(int valuationPrerequisiteId, UserInfo user)
+        {
+            var prerequisite = _context.TBL_COLLATERAL_VALUATION_PRE.Where(O => O.APPROVALSTATUSID == (int)ApprovalStatusEnum.Processing
+                                        && O.VALUATIONPREREQUISITEID == valuationPrerequisiteId).FirstOrDefault();
+
+            if (prerequisite != null)
+            {
+                prerequisite.APPROVALSTATUSID = (int)ApprovalStatusEnum.Referred;
+
+                // Audit Section ---------------------------
+                var audit = new TBL_AUDIT
+                {
+                    AUDITTYPEID = (short)AuditTypeEnum.ValuationPrerequisiteUpdated,
+                    STAFFID = user.staffId,
+                    BRANCHID = (short)user.BranchId,
+                    DETAIL = "Updated Valuation Prerequisite with code: " + prerequisite.VALUATIONPREREQUISITEID,
+                    IPADDRESS = user.userIPAddress,
+                    URL = user.applicationUrl,
+                    APPLICATIONDATE = _general.GetApplicationDate(),
+                    SYSTEMDATETIME = DateTime.Now
+                };
+
+                _audit.AddAuditTrail(audit);
+                // End of Audit Section ---------------------
+
+                return _context.SaveChanges() > 0;
+            }
+
+            return false;
+        }
+
     }
 }
