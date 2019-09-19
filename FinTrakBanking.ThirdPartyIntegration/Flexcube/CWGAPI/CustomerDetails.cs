653ef92f40cfb7db40c004ddb0e5ee38b852eddf
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2019-09-19 10:19:32 +0100

    Update

--- FinTrakBanking.ThirdPartyIntegration/Flexcube/CWGAPI/CustomerDetails.cs
+++ FinTrakBanking.ThirdPartyIntegration/Flexcube/CWGAPI/CustomerDetails.cs
@@ -250,7 +250,7 @@
 
                     var logs = new TBL_CUSTOM_API_LOGS
                     {
-                        APIURL = $"GetCustomerAccountBalance/{customerAccount}",
+                        APIURL = $"{API_URL}/GetCustomerAccountBalance/{customerAccount}",
                         LOGTYPEID = 1,
                         REFERENCENUMBER = customerAccount,
                         REQUESTDATETIME = requestDatetime,
@@ -388,7 +388,7 @@
 
                     ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
                     requestDatetime = DateTime.Now;
-                    response = await client.GetAsync($"ExposedPerson/{customerCode}");
+                    response = await client.GetAsync($"GetExposedPerson/{customerCode}");
                     responseDateTime = DateTime.Now;
                     if (response.IsSuccessStatusCode)
                     {
@@ -588,7 +588,7 @@
                     var logs = new TBL_CUSTOM_API_LOGS
                     {
                         //APIURL = $"api/InterestRateInquiry/GetInterestRateInquiry?model.accountNumber={accountNumber}&model.accountType={accountType}",
-                        APIURL = $"api / InterestRate / GetInterestRateInquiry /{ accountNumber }",
+                        APIURL = $"{API_URL}/GetInterestRateInquiry /{ accountNumber }",
                         LOGTYPEID = 18,
                         REFERENCENUMBER = accountNumber,
                         REQUESTDATETIME = requestDatetime,
@@ -734,9 +734,9 @@
                 var currentDate = DateTime.Now;
                 var startDate = DateTime.Now.AddMonths( -durationInMonths);
 
-                var month = DateTime.Now.Month - 1;
-                var year = DateTime.Now.Year;
-                var searchDate = "0"+month + "-" + year;
+                //var month = DateTime.Now.Month - 1;
+                //var year = DateTime.Now.Year;
+                //var searchDate = "0"+month + "-" + year;
 
                 HttpClientHandler handler = new HttpClientHandler();
                 HttpClient httpClientInstance;
