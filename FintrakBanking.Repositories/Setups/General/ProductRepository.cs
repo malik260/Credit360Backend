f039fd1da5533d4809b52b831ad846a2e0648bc7
ifeanyi.ikefuna <ikefunastephen@gmail.com>
2019-09-19 10:45:12 +0100

    b4pu

--- FintrakBanking.Repositories/Setups/General/ProductRepository.cs
+++ FintrakBanking.Repositories/Setups/General/ProductRepository.cs
@@ -3496,7 +3496,7 @@ namespace FintrakBanking.Repositories.Setups.General
                 entity.DELETED = true;
             }
 
-            return context.SaveChanges() > 0;
+            return docContext.SaveChanges() > 0;
         }
 
         #endregion Product Document Mapping
