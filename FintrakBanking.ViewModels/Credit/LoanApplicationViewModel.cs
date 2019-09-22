66fbb917085f331790f4a1a56264354da09da567
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2019-09-22 11:40:31 +0100

    commiting the latest copy of the back-end solution by Benjamin

--- FintrakBanking.ViewModels/Credit/LoanApplicationViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanApplicationViewModel.cs
@@ -10,6 +10,10 @@ namespace FintrakBanking.ViewModels.Credit
     public class LoanApplicationViewModel : GeneralEntity
     {
         public string approvedProductName;
+        public decimal? equityControl;
+        public int? moratrium;
+        public string pledgeCollateral;
+        public decimal valueOfCollateral;
 
         public LoanApplicationViewModel()
         {
