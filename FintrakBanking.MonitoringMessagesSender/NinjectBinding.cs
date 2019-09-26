9518154085f9b7a60c99e1c908282e58aed9d839
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2019-09-26 09:10:09 +0100

    commiting the latest copy of the back-end solution by Benjamin

--- FintrakBanking.MonitoringMessagesSender/NinjectBinding.cs
+++ FintrakBanking.MonitoringMessagesSender/NinjectBinding.cs
@@ -1,6 +1,11 @@
 ﻿using FintrakBanking.Interfaces.AlertMonitoring;
 using FintrakBanking.Repositories.AlertMonitoring;
 using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FintrakBanking.MonitoringMessagesSender
 {
@@ -8,10 +13,10 @@ namespace FintrakBanking.MonitoringMessagesSender
     {
         public override void Load()
         {
-            Bind<IEmailSender>().To<EmailSender>().InSingletonScope();
-            Bind<IAlertMessagesEngine>().To<AlertMessagesEngine>().InSingletonScope();
-            Bind<IAlertMessageLogger>().To<AlertMessageLogger>().InSingletonScope();
-            Bind<ISLANotification>().To<SLANotification>().InSingletonScope();
+            //Bind<IEmailSender>().To<EmailSender>().InSingletonScope();
+            //Bind<IAlertMessagesEngine>().To<AlertMessagesEngine>().InSingletonScope();
+            //Bind<IAlertMessageLogger>().To<AlertMessageLogger>().InSingletonScope();
+            //Bind<ISLANotification>().To<SLANotification>().InSingletonScope();
         }
     }
 }
