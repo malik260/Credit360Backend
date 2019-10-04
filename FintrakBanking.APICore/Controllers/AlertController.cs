4531e505f14856ac56b9744cd31324977215f147
Kingsley Ozoemena <kingsley.ozoemena@fintraksoftware.com>
2019-10-04 18:03:17 +0100

    updatedReport

--- FintrakBanking.APICore/Controllers/AlertController.cs
+++ FintrakBanking.APICore/Controllers/AlertController.cs
@@ -123,7 +123,7 @@ namespace FintrakBanking.APICore.Controllers
         [HttpDelete]
         [ClaimsAuthorization]
         [Route("alert-title/{id}")]
-        public HttpResponseMessage DeleteOriginalDocumentApproval(int id)
+        public HttpResponseMessage DeleteAlertTitle(int id)
         {
             UserInfo user = new UserInfo()
             {
