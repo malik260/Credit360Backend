458e875a48c9cba4ef4d36be5c8ba87c9df63685
devserver <augustine.nwaka@fintraksoftware.com>
2019-10-04 11:30:36 +0100

    LoanPerformance

--- FintrakBanking.APICore/Filters/AdministratorLockoutFilter.cs
+++ FintrakBanking.APICore/Filters/AdministratorLockoutFilter.cs
@@ -1,5 +1,6 @@
 ﻿using FintrakBanking.Entities.Models;
 using FintrakBanking.Repositories.Setups.General;
+//using FintrakBanking.Repositories.Setups.General;
 using System;
 using System.Collections.Generic;
 using System.Linq;
