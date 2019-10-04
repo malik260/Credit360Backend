51659bdb0a064c976be6e88b09c2d26a087ffc35
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2019-10-04 10:57:26 +0100

    b4pull

--- FintrakBanking.Interfaces/Setups/General/IAlertRepository.cs
+++ FintrakBanking.Interfaces/Setups/General/IAlertRepository.cs
@@ -15,5 +15,25 @@ namespace FintrakBanking.Interfaces.Setups.General
         bool DeleteAlertTitle(int id, UserInfo user);
         AlertViewModel GetAlertById(int id);
         bool UpdateAlertTitle(int id, AlertViewModel model, UserInfo user);
+        IEnumerable<AlertSetupViewModel> GetAllAlertSetup();
+        AlertSetupViewModel GetAlertSetupById(int id);
+        bool AddAlertSetup(AlertSetupViewModel model);
+        bool UpdateAlertSetup(int id, AlertSetupViewModel model, UserInfo user);
+        bool DeleteAlertSetup(int id, UserInfo user);
+        IEnumerable<LevelGroupMappingViewModel> GetAllAlertLevelGroupMapping();
+        LevelGroupMappingViewModel GetAlertLevelGroupMappingById(int id);
+        bool AddAlertLevelGroupMapping(LevelGroupMappingViewModel model);
+        bool UpdateAlertLevelGroupMapping(int id, LevelGroupMappingViewModel model, UserInfo user);
+        bool DeleteAlertLevelGroupMapping(int id, UserInfo user);
+        IEnumerable<AlertLevelGroupViewModel> GetAllAlertLevelGroup();
+        AlertLevelGroupViewModel GetAlertLevelGroupById(int id);
+        bool AddAlertLevelGroup(AlertLevelGroupViewModel model);
+        bool UpdateAlertLevelGroup(int id, AlertLevelGroupViewModel model, UserInfo user);
+        bool DeleteAlertLevelGroup(int id, UserInfo user);
+        IEnumerable<AlertLevelViewModel> GetAllAlertLevel();
+        AlertLevelViewModel GetAlertLevelById(int id);
+        bool AddAlertLevel(AlertLevelViewModel model);
+        bool UpdateAlertLevel(int id, AlertLevelViewModel model, UserInfo user);
+        bool DeleteAlertLevel(int id, UserInfo user);
     }
 }
