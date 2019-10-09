726af4309ed8c1eb26225a8596200232d73b0931
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2019-10-09 19:25:39 +0100

    rac and deferral waiver

--- FintrakBanking.ViewModels/Risk/RiskAcceptanceCriteriaViewModel.cs
+++ FintrakBanking.ViewModels/Risk/RiskAcceptanceCriteriaViewModel.cs
@@ -76,6 +76,7 @@ namespace FintrakBanking.ViewModels.Risk
 
     public class RacDefinitionViewModel : GeneralEntity
     {
+        
         public string productClassName { get; set; }
 
         public short? currencyId { get; set; }
@@ -117,6 +118,7 @@ namespace FintrakBanking.ViewModels.Risk
         public string operationName { get; set; }
         public string approvalLevelName { get; set; }
         public decimal? controlAmount { get; set; }
+        public decimal? controlAmountMax { get; set; }
         public int? controlOptionId { get; set; }
         public string controlOption { get; set; }
         public string racCategoryType { get; set; }
