424656987b7132ccb9ddb4108c1bdb3a4c809237
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2019-10-09 08:35:23 +0100

    CashFlowLending and Cash collaterized

--- FintrakBanking.Interfaces/Credit/ILoanApplicationRepository.cs
+++ FintrakBanking.Interfaces/Credit/ILoanApplicationRepository.cs
@@ -142,6 +142,7 @@ namespace FintrakBanking.Interfaces.Credit
 
         IEnumerable<RevisedProcessFlowModel> getFacilityApplicationRevisedProcessFlow();
         IEnumerable<RevisedProcessFlowModel> getFacilityApplicationRevisedProcessFlowByProductClassId(short productClassId, short productId, short productTypeId);
+        RevisedProcessFlowModel getCashCollaterizedProcessFlowBy();
 
         IEnumerable<LoanApplicationViewModel> GetFacilityByApplicationId(int loanApplicationId);
 
