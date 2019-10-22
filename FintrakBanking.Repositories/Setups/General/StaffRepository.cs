6b051e84f7eaaf71ef6b3693d5827c58a3449323
Adeyemi Olaopa <adeyemi.olaopa@fintraksoftware.com>
2019-10-22 11:48:54 +0100

    StaffRepository & WebConfig

--- FintrakBanking.Repositories/Setups/General/StaffRepository.cs
+++ FintrakBanking.Repositories/Setups/General/StaffRepository.cs
@@ -2054,7 +2054,7 @@ namespace FintrakBanking.Repositories.Setups.General
 
         public int GetNextApprovalLvlRoleId(int roleId, int groupId)
         {
-            if (groupId == 0) groupId = 261;
+            if (groupId == 0) groupId = 261; //ApprovalGroupEnum.Business
 
             var levels = context.TBL_APPROVAL_LEVEL.Where(l => l.GROUPID == groupId).ToList();
             var currentPosition = levels.Find(l => l.STAFFROLEID == roleId)?.POSITION ?? 0;
