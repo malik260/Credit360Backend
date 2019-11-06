98614d0f8cc835c8a8b4d1d9f4f3ee7a1ba9ad54
Adeyemi Olaopa <adeyemi.olaopa@fintraksoftware.com>
2019-11-06 13:05:45 +0100

    IntegrationWithFlexcube, LoanApplicationRepository, CustomerRepository & CustomerStagingRepository

--- FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
@@ -1336,8 +1336,8 @@ namespace FintrakBanking.Repositories.Credit
                 if (isCheckListDone && SubmitLoanApplicationForCam(applicationId, staffId, checkListIndex) == 1)
                 {
                     var casa = context.TBL_CASA.Find(application.CASAACCOUNTID);
-
                     var setup = context.TBL_SETUP_GLOBAL.FirstOrDefault();
+
                     if (setup.USE_THIRD_PARTY_INTEGRATION)
                     {
                         creditCommon.LoadCustomerTurnover(
