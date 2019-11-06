981b500a6c3fed03c570419d3100a2095ec7b003
ifeanyi.ikefuna <ikefunastephen@gmail.com>
2019-11-06 19:05:25 +0100

    1

---
+++ FintrakBanking.Common/Enum/AdjustedFacilityTypeEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FintrakBanking.Common.Enum
+{
+    public enum AdjustedFacilityTypeEnum
+    {
+        MORTGAGELOAN = 1,
+        OVERDRAFT = 2,
+        TERMLOAN = 3,
+        CREDITCARD = 4,
+        PERSONALLOAN = 5,
+        OTHER = 6,
+        ONLENDING = 7,
+        PROJECTFINANCELOAN = 8,
+        TERMLOANN = 9,
+        BONDGTEE = 10,
+        LC = 11,
+        GUARANTEE = 12,
+        STAFFLOAN = 13,
+        //ONLENDING = 15
+        //N/A = 16,
+        OTHERSN = 17,
+        ADVANCESUNDERLEASEN = 18,
+        TIMELOAN = 19,
+        TRADELOAN = 20,
+        ONLENDINGCBN = 21,
+        USANCE = 22,
+        FINANCELEASE = 23
+
+    }
+}
