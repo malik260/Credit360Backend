da00cb34c85f91f855b51bb70e66b86048d929fc
Adeyemi Olaopa <adeyemi.olaopa@fintraksoftware.com>
2019-11-14 15:40:45 +0100

    TransactionPosting & Overdraft

--- FintrakBanking.ViewModels/ThridPartyIntegration/TransactionPostingViewModel.cs
+++ FintrakBanking.ViewModels/ThridPartyIntegration/TransactionPostingViewModel.cs
@@ -306,7 +306,7 @@ public class LienProcessViewModel //: GeneralEntity
         public string webRequestStatus { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}")]
         public DateTime webRequestDate { get; set; }
-        public string responseCode { get; set; }
+        public string response_code { get; set; }
         public string bo_code { get; set; }
         public string bo_message { get; set; }
         public string lien_id { get; set; }
