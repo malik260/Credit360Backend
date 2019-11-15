2d21f908f39b8beae3c8408a0ca3f13df107d3e3
Kingsley Ozoemena <kingsley.ozoemena@fintraksoftware.com>
2019-11-15 11:28:17 +0100

    updated

--- FintrakBanking.APICore/Controllers/ReportsController.cs
+++ FintrakBanking.APICore/Controllers/ReportsController.cs
@@ -2884,6 +2884,30 @@ namespace FintrakBanking.APICore.Controllers
             }
         }
 
+        [HttpPost]
+        [ClaimsAuthorization]
+        [Route("copy-ifrs-classification")]
+        public HttpResponseMessage CopyOfRiskAssetByIfrsClassification([FromBody] RiskAssets obj)
+        {
+
+            var token = new TokenDecryptionHelper(); 
+            try
+            {
+                var data = repo.CopyOfRiskAssetByIfrsClassification(obj.runDate, obj.level, obj.misCode, obj.exposureType, obj.divisionName, obj.groupName, obj.branchName, obj.regionName);
+                if (data == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK,
+                    new { success = false, message = "No record found" });
+                }
+                return Request.CreateResponse(HttpStatusCode.OK,
+                    new { success = true, result = data });  //Ok(accounts);
+            }
+            catch (SecureException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new { success = false, message = ex.Message });
+            }
+        }
+
         [HttpGet]
         [ClaimsAuthorization]
         [Route("get-drawdown-memo-pdf/{referenceNumber}")]
--- FintrakBanking.ReportObjects/ReportCalls/ReportRoutes.cs
+++ FintrakBanking.ReportObjects/ReportCalls/ReportRoutes.cs
@@ -1251,6 +1251,17 @@ namespace FintrakBanking.ReportObjects.ReportCalls
             return path;
         }
 
+        public string CopyOfRiskAssetByIfrsClassification(DateTime runDate, string level, string misCode, string exposureType, string divisionName, string groupName, string branchName, string regionName)
+        {
+            HashProperty hashValue = GetHashedDateValue(dateInfor);
+
+            string path = string.Empty;
+            path = reportPath + "ReportViews/CopyOfRiskAssetByIfrsClassification.aspx?runDate=" + runDate + "&level=" + level + "&MisCode=" + misCode + "&exposureType=" + exposureType + "&divisionName=" + divisionName + "&branchName=" + branchName + "&regionName=" + regionName + "&key1=" + dateInfor + "&key2=" + hashValue.hashedDateValue + "&groupName=" + groupName;
+            return path;
+        }
+
+        
+
 
         public string RiskAssetMain1Report(DateTime runDate, string level, string misCode, string exposureType, string divisionName, string groupName, string branchName, string regionName)
         {
