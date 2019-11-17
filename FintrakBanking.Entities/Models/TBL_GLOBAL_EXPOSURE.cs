09723ab9db1a32ed800daa170eb59f9d08f0c14f
devserver <augustine.nwaka@fintraksoftware.com>
2019-11-17 17:00:43 +0100

    updat

--- FintrakBanking.Entities/Models/TBL_GLOBAL_EXPOSURE.cs
+++ FintrakBanking.Entities/Models/TBL_GLOBAL_EXPOSURE.cs
@@ -66,11 +66,11 @@ namespace FintrakBanking.Entities.AlertReportingModels
         public string INTERESTRATE { get; set; }
         public decimal? SECTIONEDLOANLIMITDIRECTFCY { get; set; }
         public decimal? SECTIONEDLOANLIMITDIRECTLCY { get; set; }
-        public decimal? TOTALEXPOSULCYPREVIOUSYEAR { get; set; }
-        public decimal? TOTAEXPOSURELCYPREVIOUS6MONTHS { get; set; }
-        public decimal? TOTAEXPOSURELCYPREVIOUS3MONTHS { get; set; }
-        public decimal? TOTAEXPOSURELCYPREVIOUSMONTHS { get; set; }
-        public decimal? TOTAEXPOSURELCYPREVIOUSDAY { get; set; }
+        //public decimal? TOTALEXPOSULCYPREVIOUSYEAR { get; set; }
+        //public decimal? TOTAEXPOSURELCYPREVIOUS6MONTHS { get; set; }
+        //public decimal? TOTAEXPOSURELCYPREVIOUS3MONTHS { get; set; }
+        //public decimal? TOTAEXPOSURELCYPREVIOUSMONTHS { get; set; }
+        //public decimal? TOTAEXPOSURELCYPREVIOUSDAY { get; set; }
         public decimal? TOTALEXPOSURE { get; set; }
         public decimal? IMPAIRMENTAMOUNT { get; set; }
         public decimal? TOTALUNPAIDOBLIGATION { get; set; }
