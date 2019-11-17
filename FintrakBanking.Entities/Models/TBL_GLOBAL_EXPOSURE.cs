c445de05d209e96d73c23be8a743a51fd9c844d5
devserver <augustine.nwaka@fintraksoftware.com>
2019-11-17 17:28:30 +0100

    updat

--- FintrakBanking.Entities/Models/TBL_GLOBAL_EXPOSURE.cs
+++ FintrakBanking.Entities/Models/TBL_GLOBAL_EXPOSURE.cs
@@ -65,7 +65,7 @@ namespace FintrakBanking.Entities.AlertReportingModels
         public float? SHF { get; set; }
         public string INTERESTRATE { get; set; }
         //public decimal? SECTIONEDLOANLIMITDIRECTFCY { get; set; }
-        public decimal? SECTIONEDLOANLIMITDIRECTLCY { get; set; }
+        //public decimal? SECTIONEDLOANLIMITDIRECTLCY { get; set; }
         //public decimal? TOTALEXPOSULCYPREVIOUSYEAR { get; set; }
         //public decimal? TOTAEXPOSURELCYPREVIOUS6MONTHS { get; set; }
         //public decimal? TOTAEXPOSURELCYPREVIOUS3MONTHS { get; set; }
