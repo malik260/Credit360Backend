d82bc79f5e6ed7078e54885f4dbcc504302c6561
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2019-12-10 19:55:50 +0100

    refer back ed to legal

--- FintrakBanking.APICore/Controllers/CreditOperationsController.cs
+++ FintrakBanking.APICore/Controllers/CreditOperationsController.cs
@@ -150,6 +150,29 @@ namespace FintrakBanking.APICore.Controllers
 
         [HttpGet]
         [ClaimsAuthorization]
+        [Route("loan-review-approval-operationtype/")]
+        public HttpResponseMessage GetReviewApprovalOperationTypeByLoanId(int productTypeId, int scheduleTypeId)
+        {
+            try
+            {
+                var data = repo.GetReviewApprovalOperationTypeByLoanId((LoanProductTypeEnum)productTypeId, (LoanScheduleTypeEnum)scheduleTypeId);
+                if (data == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK,
+                       new { success = false, message = "No record found" });
+                }
+                return Request.CreateResponse(HttpStatusCode.OK,
+                       new { success = true, result = data });
+            }
+            catch (SecureException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK,
+                      new { success = false, message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [ClaimsAuthorization]
         [Route("loan-search/")]
         public HttpResponseMessage SearchForLoan(string searchQuery)
         {
