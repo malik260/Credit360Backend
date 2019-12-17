ba91cbfa7d5b8d1e48b7922a3ece600218c6a1dd
superuser <augustine.nwaka@fintraksoftware.com>
2019-12-17 11:23:57 +0100

    b4 pull

--- FintrakBanking.Entities/Models/TBL_STAFF.cs
+++ FintrakBanking.Entities/Models/TBL_STAFF.cs
@@ -122,7 +122,7 @@ namespace FintrakBanking.Entities.Models
         public string NOKRELATIONSHIP { get; set; }
 
         //[StringLength(100), Column(name: "COMMENT_")]
-        public string COMMENT { get; set; }
+        public string COMMENT_ { get; set; }
 
         public short? BRANCHID { get; set; }
 
