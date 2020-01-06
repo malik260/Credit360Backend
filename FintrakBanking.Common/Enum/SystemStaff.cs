30b4694d6f29f62cc6b4ef333f79f07470e10ba5
superuser <augustine.nwaka@fintraksoftware.com>
2020-01-06 15:35:32 +0100

    3

--- FintrakBanking.Common/Enum/SystemStaff.cs
+++ FintrakBanking.Common/Enum/SystemStaff.cs
@@ -6,8 +6,12 @@ namespace FintrakBanking.Common.Enum
 {
     public enum SystemStaff
     {
-        System  = -1,
+        System  = 1,
         HQ = 1
+
+            //System = -1,
+       // HQ = 1
+
 
     }
 }
