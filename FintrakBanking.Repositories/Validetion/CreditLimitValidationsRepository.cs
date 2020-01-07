e4629dba546b93b695a810a8aea82e74f3581067
Kingsley Ozoemena <kingsley.ozoemena@fintraksoftware.com>
2020-01-07 12:10:43 +0100

    update

--- FintrakBanking.Repositories/Validetion/CreditLimitValidationsRepository.cs
+++ FintrakBanking.Repositories/Validetion/CreditLimitValidationsRepository.cs
@@ -697,7 +697,7 @@ namespace FintrakBanking.Repositories.CreditLimitValidations
             if (customerIds.Count() <= 0) return false;
             foreach (var id in customerIds)
             {
-                var bvn = context.TBL_CUSTOMER.Find(id).CUSTOMERBVN;
+                var bvn = context.TBL_CUSTOMER.Find(id)?.CUSTOMERBVN;
                 var isDirector = context.TBL_COMPANY_DIRECTOR.Any(d => d.BVN.Trim() == bvn.Trim());
                 if (isDirector) return isDirector;
             }
@@ -706,7 +706,7 @@ namespace FintrakBanking.Repositories.CreditLimitValidations
 
         public bool CustomerIsDirector(int? customerId)
         {
-            var bvn = context.TBL_CUSTOMER.Find(customerId).CUSTOMERBVN;
+            var bvn = context.TBL_CUSTOMER.Find(customerId)?.CUSTOMERBVN;
             if (String.IsNullOrEmpty(bvn) || String.IsNullOrEmpty(bvn)) return false;
             var isDirector = context.TBL_COMPANY_DIRECTOR.Any(d => d.BVN.Trim() == bvn.Trim());
             if (isDirector) return isDirector;
