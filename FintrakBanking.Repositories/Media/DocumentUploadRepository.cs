fa1f399f55ade9d4242ac8cc3587507c2d025b09
superuser <augustine.nwaka@fintraksoftware.com>
2020-01-14 21:02:04 +0100

    Before Pull

--- FintrakBanking.Repositories/Media/DocumentUploadRepository.cs
+++ FintrakBanking.Repositories/Media/DocumentUploadRepository.cs
@@ -64,19 +64,76 @@ namespace FintrakBanking.Repositories.Media
 
         public IEnumerable<DocumentUploadViewModel> GetDocumentUploads(int staffId, int operationId, int targetId, bool isOperationSpecific=false)
         {
-            
+
+            var firstQuery = docContext.TBL_DOCUMENT_USAGE.Where(x => x.DELETED == false && x.OPERATIONID == operationId && x.TARGETID == targetId)
+                 .Join(docContext.TBL_DOCUMENT_UPLOAD.Where(x => x.DELETED == false)
+                 , us => us.DOCUMENTUPLOADID, up => up.DOCUMENTUPLOADID, (us, up) =>
+                new {
+                    documentUploadId = up.DOCUMENTUPLOADID,
+                    fileName = up.FILENAME,
+                    fileExtension = up.FILEEXTENSION,
+                    fileSize = up.FILESIZE,
+                    fileSizeUnit = up.FILESIZEUNIT,
+                    fileData = up.FILEDATA,
+                    companyId = up.COMPANYID,
+                    issueDate = up.ISSUEDATE,
+                    expiryDate = up.EXPIRYDATE,
+                    physicalFilenumber = up.PHYSICALFILENUMBER,
+                    physicalLocation = up.PHYSICALLOCATION,
+                    documentTypeId = up.DOCUMENTTYPEID,
+                    documentTypeName = up.TBL_DOCUMENT_TYPE.DOCUMENTTYPENAME,
+                    documentCategoryId = up.TBL_DOCUMENT_TYPE.DOCUMENTCATEGORYID,
+                    documentCategoryName = up.TBL_DOCUMENT_TYPE.TBL_DOCUMENT_CATEGORY.DOCUMENTCATEGORYNAME,
+                    owner = up.CREATEDBY == staffId,
+                    dateTimeCreated = us.DATETIMECREATED,
+                    dateTimeUpdated = us.DATETIMEUPDATED,
+                    createdBy = us.CREATEDBY.Value,
+                }
+                 )
+                 .AsEnumerable()
+                 .Select(up => new DocumentUploadViewModel
+                 {
+                     documentUploadId = up.documentUploadId,
+                     fileName = up.fileName,
+                     fileExtension = up.fileExtension,
+                     fileSize = up.fileSize,
+                     fileSizeUnit = up.fileSizeUnit,
+                     fileData = up.fileData,
+                     companyId = up.companyId,
+                     issueDate = up.issueDate,
+                     expiryDate = up.expiryDate,
+                     physicalFilenumber = up.physicalFilenumber,
+                     physicalLocation = up.physicalLocation,
+                     documentTypeId = up.documentTypeId,
+                     documentTypeName = up.documentTypeName,
+                     documentCategoryId = up.documentCategoryId,
+                     documentCategoryName = up.documentCategoryName,
+                     owner = up.owner,
+                     dateTimeCreated = up.dateTimeCreated,
+                     dateTimeUpdated = up.dateTimeUpdated,
+                     createdBy = up.createdBy,
+                     uploadedBy = context.TBL_STAFF.Where(s => s.STAFFID == up.createdBy && s.DELETED != true).Select(s => s.FIRSTNAME + " " + s.LASTNAME + " " + "(" + s.STAFFCODE + ")").FirstOrDefault(),
+
+                 })
+                 .OrderBy(x => x.dateTimeCreated)
+                 .ThenBy(x => x.documentCategoryId)
+                 .ThenBy(x => x.documentTypeId)?
+                 .ToList();
+
+
+
             var customerCreditBureau = (from ccb in context.TBL_CUSTOMER_CREDIT_BUREAU
                                         join app in context.TBL_LOAN_APPLICATION_DETAIL on ccb.CUSTOMERID equals app.CUSTOMERID
                                         where app.LOANAPPLICATIONID == targetId
                                         select ccb.CUSTOMERCREDITBUREAUID).ToList();
 
             string staff = (from x in context.TBL_STAFF
-                           join app in context.TBL_LOAN_APPLICATION_DETAIL on x.STAFFID equals app.CREATEDBY
-                           where app.LOANAPPLICATIONID == targetId
-                           select x.FIRSTNAME + " " + x.LASTNAME).FirstOrDefault();
-          
+                            join app in context.TBL_LOAN_APPLICATION_DETAIL on x.STAFFID equals app.CREATEDBY
+                            where app.LOANAPPLICATIONID == targetId
+                            select x.FIRSTNAME + " " + x.LASTNAME).FirstOrDefault();
+
             var secondQuery = (from d in docContext.TBL_CUSTOMER_CREDIT_BUREAU
-                               where customerCreditBureau.Contains(d.CUSTOMERCREDITBUREAUID)  
+                               where customerCreditBureau.Contains(d.CUSTOMERCREDITBUREAUID)
                                select new DocumentUploadViewModel
                                {
                                    documentUploadId = d.DOCUMENTID,
@@ -91,7 +148,7 @@ namespace FintrakBanking.Repositories.Media
                                    //fileSize = d.fileSize,
                                })?.ToList();
 
-            var output = secondQuery.ToList();
+            var output = firstQuery.Union(secondQuery).ToList();
 
             if (!isOperationSpecific)
             {
