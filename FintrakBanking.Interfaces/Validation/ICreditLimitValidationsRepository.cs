25866eec80ee098e94b31fbee4714a6c3bd629e2
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-01-22 19:43:22 +0100

    b4 pull

--- FintrakBanking.Interfaces/Validation/ICreditLimitValidationsRepository.cs
+++ FintrakBanking.Interfaces/Validation/ICreditLimitValidationsRepository.cs
@@ -35,8 +35,6 @@ namespace FintrakBanking.Interfaces.CreditLimitValidations
         CreditLimitValidationsModel ValidateAmountBySegment(short segmentId);
         CreditLimitValidationsModel ValidateNPLBySegment(short segmentId);
         CreditLimitValidationsModel ValidateSingleObligorLimit(LoanApplicationViewModel application);
-        CreditLimitValidationsModel ValidateObligorRistRating(LoanApplicationViewModel application);
-
         IEnumerable<ObligorLimitViewModel> GetAllObligorLimit();
         bool ValidateRiskRating(string riskRating);
         bool AddUpdateRiskRating(ObligorLimitViewModel entity);
