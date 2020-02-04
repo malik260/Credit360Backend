67c10c9dfe0a665e1e91ac8acfe485005d20ac52
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-02-04 14:33:10 +0100

    b4 pull

--- FintrakBanking.Repositories/Credit/LoanRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanRepository.cs
@@ -13061,6 +13061,10 @@ namespace FintrakBanking.Repositories.Credit
 
                                    });
             var j = allFilteredLoan.ToList();
+            //if(j == null)
+            //{
+            //    allFilteredLoan = SearchRevolvingLoan(searchQuery);
+            //}
             return allFilteredLoan;
         }
 
