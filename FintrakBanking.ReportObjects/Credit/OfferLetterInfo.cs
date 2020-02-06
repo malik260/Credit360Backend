70a90029b9dfac4640fc589ed0e1be268bc81b47
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-02-06 17:24:38 +0100

    b4 pull

--- FintrakBanking.ReportObjects/Credit/OfferLetterInfo.cs
+++ FintrakBanking.ReportObjects/Credit/OfferLetterInfo.cs
@@ -203,16 +203,16 @@ namespace FintrakBanking.ReportObjects.Credit
             {
                 var loanDetails = (from a in context.TBL_LOAN_APPLICATION
                                    join b in context.TBL_LOAN_APPLICATION_DETAIL on a.LOANAPPLICATIONID equals b.LOANAPPLICATIONID
-                                   join c in context.TBL_CUSTOMER on a.CUSTOMERID equals c.CUSTOMERID into cc
-                                   from c in cc.DefaultIfEmpty()
-                                   join d in context.TBL_CUSTOMER_GROUP on a.CUSTOMERGROUPID equals d.CUSTOMERGROUPID into cg
-                                   from d in cg.DefaultIfEmpty()
-                                   join e in context.TBL_CUSTOMER_ADDRESS on a.CUSTOMERID equals e.CUSTOMERID into dg
-                                   from e in dg.DefaultIfEmpty()
-                                   join g in context.TBL_CUSTOMER_PHONECONTACT on a.CUSTOMERID equals g.CUSTOMERID into gg
-                                   from g in gg.DefaultIfEmpty()
-                                   join h in context.TBL_CURRENCY on b.CURRENCYID equals h.CURRENCYID into hh
-                                   from h in hh.DefaultIfEmpty()
+                                   join c in context.TBL_CUSTOMER on a.CUSTOMERID equals c.CUSTOMERID
+                                   //from c in cc.DefaultIfEmpty()
+                                   //join d in context.TBL_CUSTOMER_GROUP on a.CUSTOMERGROUPID equals d.CUSTOMERGROUPID into cg
+                                   //from d in cg.DefaultIfEmpty()
+                                   //join e in context.TBL_CUSTOMER_ADDRESS on a.CUSTOMERID equals e.CUSTOMERID into dg
+                                   //from e in dg.DefaultIfEmpty()
+                                   //join g in context.TBL_CUSTOMER_PHONECONTACT on a.CUSTOMERID equals g.CUSTOMERID into gg
+                                   //from g in gg.DefaultIfEmpty()
+                                   //join h in context.TBL_CURRENCY on b.CURRENCYID equals h.CURRENCYID into hh
+                                   //from h in hh.DefaultIfEmpty()
                                    where a.APPLICATIONREFERENCENUMBER == applicationRefNumber &&
                                          b.STATUSID == (int)ApprovalStatusEnum.Approved
                                    select new OfferLetterDetailViewModel()
@@ -223,27 +223,27 @@ namespace FintrakBanking.ReportObjects.Credit
                                        approvedProductId = b.APPROVEDPRODUCTID,
                                        productClassId = a.PRODUCTCLASSID,
                                        productTypeId = context.TBL_PRODUCT.FirstOrDefault(x => x.PRODUCTID == b.APPROVEDPRODUCTID).PRODUCTTYPEID,
-                                       currencyName = h.CURRENCYCODE,//b.TBL_CURRENCY.CURRENCYNAME,
+                                       currencyName = b.TBL_CURRENCY.CURRENCYNAME,
                                        tenor = b.APPROVEDTENOR,
                                        //approvedTenorString = b.APPROVEDTENOR,
                                        interestRate = b.APPROVEDINTERESTRATE,
-                                       loanAmount = b.APPROVEDAMOUNT,
+                                       loanAmount = a.APPROVEDAMOUNT,
                                        exchangeRate = b.EXCHANGERATE,
                                        currencyId = b.CURRENCYID,
                                        companyName = context.TBL_COMPANY.Where(x => x.COMPANYID == a.COMPANYID).Select(x => x.NAME).FirstOrDefault(),
-                                       customerName = a.LOANAPPLICATIONTYPEID == 1 ? c.TITLE + " " + c.FIRSTNAME + " " + c.LASTNAME : d.GROUPNAME + " - " + d.GROUPCODE,
-                                       customerAddress = e.ADDRESS ?? " ", //a.TBL_CUSTOMER.TBL_CUSTOMER_ADDRESS.FirstOrDefault().ADDRESS ?? string.Empty,
+                                       customerName = a.LOANAPPLICATIONTYPEID == 1 ? c.TITLE + " " + c.FIRSTNAME + " " + c.LASTNAME : a.TBL_CUSTOMER_GROUP.GROUPNAME + " - " + a.TBL_CUSTOMER_GROUP.GROUPCODE,
+                                       customerAddress = a.TBL_CUSTOMER.TBL_CUSTOMER_ADDRESS.FirstOrDefault().ADDRESS ?? string.Empty,
                                        applicationDate = a.APPLICATIONDATE,
-                                       customerGroupName = d.GROUPNAME + " - " + d.GROUPCODE,
+                                       customerGroupName = a.TBL_CUSTOMER_GROUP.GROUPNAME + " - " + a.TBL_CUSTOMER_GROUP.GROUPCODE,
                                        customerEmailAddress = a.TBL_CUSTOMER.EMAILADDRESS,
-                                       customerPhoneNumber = g.PHONENUMBER,//a.TBL_CUSTOMER.TBL_CUSTOMER_PHONECONTACT.FirstOrDefault().PHONENUMBER,
+                                       customerPhoneNumber = a.TBL_CUSTOMER.TBL_CUSTOMER_PHONECONTACT.FirstOrDefault().PHONENUMBER,
                                        loanApplicationId = applicationRefNumber,
                                        repaymentSchedule = b.TBL_REPAYMENT_TERM.REPAYMENTTERMDETAIL ?? "Not applicable",
                                        repaymentTerms = b.REPAYMENTTERMS ?? "Not applicable",
                                        purpose = b.LOANPURPOSE,
                                        productPriceIndex = b.PRODUCTPRICEINDEXID != null ? "+ " + context.TBL_PRODUCT_PRICE_INDEX.Where(x => x.PRODUCTPRICEINDEXID == b.PRODUCTPRICEINDEXID).Select(x => x.PRICEINDEXNAME).FirstOrDefault() : "",
                                        newApplicationDate = context.TBL_FINANCECURRENTDATE.FirstOrDefault().CURRENTDATE,
-                                       //GetLoanApplicationFee(applicationRefNumber, b.LOANAPPLICATIONDETAILID)
+                                     
                                    }).ToList();
                 //var fees = (from df in context.TBL_LOAN_APPLICATION_DETL_FEE
                 //            join ad in context.TBL_LOAN_APPLICATION_DETAIL on df.LOANAPPLICATIONDETAILID equals ad.LOANAPPLICATIONDETAILID
@@ -395,7 +395,7 @@ namespace FintrakBanking.ReportObjects.Credit
                                    currencyName = b.TBL_CURRENCY.CURRENCYNAME,
                                    tenor = b.APPROVEDTENOR,
                                    interestRate = b.APPROVEDINTERESTRATE,
-                                   loanAmount = b.APPROVEDAMOUNT,
+                                   loanAmount = a.APPROVEDAMOUNT,
                                    exchangeRate = b.EXCHANGERATE,
                                    loanTypeName = a.TBL_LOAN_APPLICATION_TYPE.LOANAPPLICATIONTYPENAME,
                                    productPriceIndex = b.PRODUCTPRICEINDEXID != null ? "+ " + context.TBL_PRODUCT_PRICE_INDEX.Where(x => x.PRODUCTPRICEINDEXID == b.PRODUCTPRICEINDEXID).Select(x => x.PRICEINDEXNAME).FirstOrDefault() : "",
