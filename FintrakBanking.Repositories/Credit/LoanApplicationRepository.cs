10463140dd7ef27b0f49de0104aad045121466e7
Adeyemi Olaopa <adeyemi.olaopa@fintraksoftware.com>
2020-02-14 10:15:17 +0100

    LMS Application Details on Appraisal

--- FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanApplicationRepository.cs
@@ -1367,34 +1367,26 @@ namespace FintrakBanking.Repositories.Credit
 
                     if (setup.USE_THIRD_PARTY_INTEGRATION)
                     {
-                        creditCommon.LoadCustomerRatios(
-                             applicationId,
-                             loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
-                             staffId
-                        );
+                        //creditCommon.LoadCustomerRatios(
+                        //     applicationId,
+                        //     loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
+                        //     staffId
+                        //);
 
-                        creditCommon.LoadCustomerGroupRatios(
-                             applicationId,
-                             loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
-                             staffId
-                        );
+                        //creditCommon.LoadCustomerGroupRatios(
+                        //     applicationId,
+                        //     loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
+                        //     staffId
+                        //);
 
-                        creditCommon.GetCorporateCustomerRating(
-                             applicationId,
-                             loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
-                             staffId
-                        );
+                        //creditCommon.GetCorporateCustomerRating(
+                        //     applicationId,
+                        //     loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
+                        //     staffId
+                        //);
 
-                        AddFacilityRating(loanApplicationDetails.ToList(), staffId);
+                        //AddFacilityRating(loanApplicationDetails.ToList(), staffId);
 
-                        //if (casa != null)
-                        //{
-                        //    creditCommon.LoadCustomerTurnover(
-                        //        applicationId,
-                        //        loanApplicationDetails.Select(x => x.CUSTOMERID).Distinct().ToList(),
-                        //        staffId
-                        //    );
-                        //}
                     }
 
                     return new LoanApplicationUpdateMessage
