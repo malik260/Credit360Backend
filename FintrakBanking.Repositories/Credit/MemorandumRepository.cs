abd4d4f599fe5d7d5827754c10d50959dcc1f773
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-03-06 09:18:27 +0100

    b4 pull

--- FintrakBanking.Repositories/Credit/MemorandumRepository.cs
+++ FintrakBanking.Repositories/Credit/MemorandumRepository.cs
@@ -4445,6 +4445,7 @@ namespace FintrakBanking.Repositories.Credit
             if (vote == 2) return "Accept";
             if (vote == 3) return "Decline";
             if (vote == 4) return "Accept";
+            if (vote == 5) return "Refered";
             return String.Empty;
         }
 
