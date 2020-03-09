eabcc0cc8f4c5a01b08b3ca74ece01b52f829503
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-03-09 20:11:30 +0100

    b4 pull

--- FintrakBanking.Entities/Models/TBL_LMSR_APPLICATION_DETAIL.cs
+++ FintrakBanking.Entities/Models/TBL_LMSR_APPLICATION_DETAIL.cs
@@ -19,7 +19,7 @@
 
         public int LOANAPPLICATIONID { get; set; }
         public int LOANID { get; set; }
-        public short CURRENCYID { get; set; }
+        public short? CURRENCYID { get; set; }
         public short PRODUCTID { get; set; }
         public int OPERATIONID { get; set; }
         public short APPROVEDLINESTATUSID { get; set; }
