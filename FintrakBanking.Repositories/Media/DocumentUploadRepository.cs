0457df883a7e2c8b53eaa71f1b26f4f72b119e7c
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-03-16 12:07:24 +0100

    b4 update

--- FintrakBanking.Repositories/Media/DocumentUploadRepository.cs
+++ FintrakBanking.Repositories/Media/DocumentUploadRepository.cs
@@ -511,7 +511,7 @@ namespace FintrakBanking.Repositories.Media
                               where app.LOANAPPLICATIONID == targetId
                               select ccb.CUSTOMERID).ToList();
 
-            var customerBureauLog = creditBureau.GetCustomerCreditBureauReportLogDeleted(customerId.FirstOrDefault(), null).Select(x => x.customerCreditBureauId);
+            var customerBureauLog = creditBureau.GetCustomerCreditBureauReportLogDeleted(customerId.FirstOrDefault(), null).Select(x => x.customerCreditBureauId).ToList();
 
             var staff = (from x in context.TBL_STAFF
                          join app in context.TBL_LOAN_APPLICATION_DETAIL on x.STAFFID equals app.CREATEDBY
@@ -519,7 +519,7 @@ namespace FintrakBanking.Repositories.Media
                          select x).FirstOrDefault();
 
             var secondQuery = (from d in docContext.TBL_CUSTOMER_CREDIT_BUREAU
-                               where customerBureauLog.Contains(d.CUSTOMERCREDITBUREAUID)
+                               where customerBureauLog?.Contains(d.CUSTOMERCREDITBUREAUID)
                                select new DocumentUploadViewModel
                                {
                                    documentUploadId = d.DOCUMENTID,
