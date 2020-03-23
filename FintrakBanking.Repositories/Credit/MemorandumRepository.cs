cc9b41861805777a6b8f799d9d2f4e1bc813452d
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-03-23 09:56:24 +0100

    b4 pull

--- FintrakBanking.Repositories/Credit/MemorandumRepository.cs
+++ FintrakBanking.Repositories/Credit/MemorandumRepository.cs
@@ -1256,7 +1256,7 @@ namespace FintrakBanking.Repositories.Credit
                 <table style='font face: arial; size:12px' border=1 width=900 cellpadding=0 cellspacing=0>
                     <tr>
                         <td></td>
-                        <td><img src='http://10.1.7.41:8082/assets/access.jpg' alt='Access Bank' width='150' height='100'></td>
+                        <td><img src='http://localhost:4200/assets/access.jpg' alt='Access Bank' width='150' height='100'></td>
                     </tr>
                     <tr>
                         <td><b>Reference Number:</b></td>
