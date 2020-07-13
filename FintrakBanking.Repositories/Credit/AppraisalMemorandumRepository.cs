9dba7121c33a993adfc3179a7365ad8f55f90473
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2020-07-13 22:46:10 +0100

    merge conflict

--- FintrakBanking.Repositories/Credit/AppraisalMemorandumRepository.cs
+++ FintrakBanking.Repositories/Credit/AppraisalMemorandumRepository.cs
@@ -3430,16 +3430,12 @@ namespace FintrakBanking.Repositories.Credit
                         OSNAME = CommonHelpers.FriendlyName()
                     };
                     this.audit.AddAuditTrail(audit);
-                    saved = context.SaveChanges() > 0;
-                    if (saved)
-                    {
-                        trans.Commit();
-                        return saved;
-                    }
+
+                    trans.Commit();
                 }
-                trans.Rollback();
             }
-            return saved;
+
+            return context.SaveChanges() > 0;
         }
 
         public List<PendingProductProgramViewModel> GetPendingProductProgram(UserInfo user)
