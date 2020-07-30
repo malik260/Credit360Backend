78c481ac35857da69da2e4a4859b8581e530fd89
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2020-07-30 20:59:06 +0100

    CRMS Code Capture refer back

--- FintrakBanking.Repositories/Credit/LoanRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanRepository.cs
@@ -9178,7 +9178,7 @@ namespace FintrakBanking.Repositories.Credit
                                        productName = d.TBL_PRODUCT.PRODUCTNAME,
                                        casaAccountId = s.CASAACCOUNTID,
                                        casaAccountId2 = s.CASAACCOUNTID2,
-                                       systemArrivalDateTime = atrail.SYSTEMARRIVALDATETIME,
+                                       //systemArrivalDateTime = atrail.SYSTEMARRIVALDATETIME,
                                        interestRate = d.APPROVEDINTERESTRATE,
                                        approvedInterestRate = d.APPROVEDINTERESTRATE,
                                        approvedAmount = d.APPROVEDAMOUNT,
