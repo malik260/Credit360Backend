6d6d22fe0a148a744242fff0f3855575c8f3a392
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-08-02 17:38:48 +0100

    b4 pull

--- FintrakBanking.ViewModels/Credit/LoanReviewOperationViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanReviewOperationViewModel.cs
@@ -230,6 +230,7 @@ namespace FintrakBanking.ViewModels.Credit
         public string loanReferenceNumber { get; set; }
         public string applicationReferenceNumber { get; set; }
         public int tenor { get { return (this.maturityDate - this.effectiveDate).Days; } }
+  
         public short principalFrequencyTypeId { get; set; }
         public short interestFrequencyTypeId { get; set; }
         public int principalNumberOfInstallment { get; set; }
@@ -418,13 +419,16 @@ public int? newInterestFrequencyTypeId { get; set; }
         public string nameOfRecoveryAgent { get; set; }
         public string address { get; set; }
         public string telephoneNumber { get; set; }
-        public DateTime? expectedRecoveryDate { get; set; }
+        public DateTime expectedRecoveryDate { get; set; }
+        public int periodToRecover { get { return (this.expectedRecoveryDate.Date - this.dateOfAssignment.Date).Days; } }
         public decimal amountRecovered { get; set; }
         public string accountNumber { get; set; }
         public string accountName { get; set; }
         public DateTime dateOfEngagement { get; set; }
         public double accountBalance { get; set; }
         public List<string> listOfAccountAssigned { get; set; }
+        public string email { get; set; }
+        public DateTime dateOfAssignment { get; set; }
     }
 
 
