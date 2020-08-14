f05b1b474caad2064c8196f416495df994744792
Nwaka Augustine <augustine.nwaka@fintraksoftware.com>
2020-08-14 14:19:24 +0100

    merge

--- FintrakBanking.Repositories/Credit/MemorandumRepository.cs
+++ FintrakBanking.Repositories/Credit/MemorandumRepository.cs
@@ -792,7 +792,7 @@ namespace FintrakBanking.Repositories.Credit
                 this.loanApplication = context.TBL_LOAN_APPLICATION.Find(loanAppId);
             }
             if (loanApplication != null)
-            {
+          {
                 if (loanApplication.LOANAPPLICATIONTYPEID == (int)LoanTypeEnum.CustomerGroup)
                 {
                     this.customerName = loanApplication.TBL_CUSTOMER_GROUP.GROUPNAME;
