d3f77aa0397b60e6a61a0fd7f2d02e9d56f5f547
Omatsola Isaac Sobotie <omatsola.sobotie@fintraksoftware.com>
2020-09-15 15:47:36 +0100

    fixed collateral due for visitation report

--- FintrakBanking.ReportObjects/ReportingObjects/LimitsMonitoringReportsObjects.cs
+++ FintrakBanking.ReportObjects/ReportingObjects/LimitsMonitoringReportsObjects.cs
@@ -154,10 +154,10 @@ namespace FintrakBanking.ReportObjects.ReportingObjects
                                                      join f in context.TBL_COLLATERAL_IMMOVE_PROPERTY on a.COLLATERALCUSTOMERID equals f.COLLATERALCUSTOMERID
                                                      join v in context.TBL_COLLATERAL_VISITATION on a.COLLATERALCUSTOMERID equals v.COLLATERALCUSTOMERID
                                                      where v.VISITATIONDATE >= startDate && v.VISITATIONDATE <= endDate
-                                                     && d.REQUIREVISITATION == true
-                                                                  orderby v.VISITATIONDATE descending
+                                                           && d.REQUIREVISITATION == true
+                                                     orderby v.VISITATIONDATE descending
 
-                                                                  select new CollateralViewModel
+                                                     select new CollateralViewModel
                                                      {
                                                          collateralTypeId = a.COLLATERALTYPEID,
                                                          collateralType = d.COLLATERALTYPENAME,
