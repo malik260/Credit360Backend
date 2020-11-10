b7dee8fef54076bd8f5b01488d3f1cf6eb4fbb37
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-11-10 14:56:11 +0100

    review

--- FintrakBanking.Repositories/Risk/RiskAcceptanceCriteriaRepository.cs
+++ FintrakBanking.Repositories/Risk/RiskAcceptanceCriteriaRepository.cs
@@ -71,7 +71,7 @@ namespace FintrakBanking.Repositories.Risk
                 var productClass = context.TBL_PRODUCT.Where(x => x.PRODUCTID == model.productId).FirstOrDefault();
                 var racDefinitionOnProduct = context.TBL_RAC_DEFINITION.Where(x => x.PRODUCTID == model.productId
                                                                                                 && x.SEARCHPLACEHOLDER == "PRODUCT"
-                                                                                                && allEmployeeType.Contains(x.EMPLOYMENTTYPE)
+                                                                                                && !allEmployeeType.Contains(x.EMPLOYMENTTYPE)
                                                                                                 && x.SHOWATDRAWDOWN == model.isDrawdown
                                                                                                 && x.ISACTIVE == true
                                                                                                 && x.DELETED == false).ToList();
@@ -79,7 +79,7 @@ namespace FintrakBanking.Repositories.Risk
                         var racDefinitionOnProductClass = context.TBL_RAC_DEFINITION.Where(x => x.PRODUCTCLASSID == model.productClassId
                                                                                                   && x.SEARCHPLACEHOLDER == "PRODUCTCLASS"
                                                                                                   && x.SHOWATDRAWDOWN == model.isDrawdown
-                                                                                                  && allEmployeeType.Contains(x.EMPLOYMENTTYPE)
+                                                                                                  && !allEmployeeType.Contains(x.EMPLOYMENTTYPE)
                                                                                                   && x.ISACTIVE == true && x.DELETED == false).ToList();
                     // var productRac = racDefinitionOnProduct.Union(racDefinitionOnProductClass);
                     //racDefinition.AddRange(productRac);
