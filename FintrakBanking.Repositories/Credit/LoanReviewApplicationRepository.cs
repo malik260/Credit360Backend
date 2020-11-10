cd1a0d1cfb133f1609a52fb29ef625ea652ae330
ifeanyi.ikefuna <ikefunastephen@gmail.com>
2020-11-10 17:22:35 +0100

    Fixes

--- FintrakBanking.Repositories/Credit/LoanReviewApplicationRepository.cs
+++ FintrakBanking.Repositories/Credit/LoanReviewApplicationRepository.cs
@@ -1625,6 +1625,7 @@ namespace FintrakBanking.Repositories.Credit
                     workflow.DeferredExecution = true;
                     workflow.IsFlowTest = model.isFlowTest;
                     workflow.IsFromPc = model.isFromPc;
+                    workflow.Tenor = operationIsTenorExtension ? lmsrDetail.Max(d => d.APPROVEDTENOR) : 0;
                     workflow.Tenor = lmsrDetail.Max(d => d.APPROVEDTENOR);
                     workflow.IgnorePostApprovalReviewer = true;
                     workflow.LevelBusinessRule = new LevelBusinessRule
@@ -1641,7 +1642,7 @@ namespace FintrakBanking.Repositories.Credit
                         WithInstruction = appl.WITHINSTRUCTION ?? false,
                         //OrrBasedApproval = appl.ISORRBASEDAPPROVAL ?? false,
                         DomiciliationNotInPlace = appl.DOMICILIATIONNOTINPLACE ?? false,
-                        tenor = lmsrDetail.Max(d => d.APPROVEDTENOR),
+                        tenor = operationIsTenorExtension ? lmsrDetail.Max(d => d.APPROVEDTENOR) : 0,
                     };
 
                     if (model.receiverLevelId == 0) workflow.NextLevelId = null;
