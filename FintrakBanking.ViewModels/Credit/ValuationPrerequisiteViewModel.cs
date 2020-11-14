9637f78a0712caf3ad80b6cef9cce5a11b3a1354
Benjamin Gbaaikye <benjamin.gbaaikye@fintraksoftware.com>
2020-11-15 00:08:50 +0100

    review

--- FintrakBanking.ViewModels/Credit/ValuationPrerequisiteViewModel.cs
+++ FintrakBanking.ViewModels/Credit/ValuationPrerequisiteViewModel.cs
@@ -52,5 +52,7 @@ namespace FintrakBanking.ViewModels.Credit
         public string customerAccount { get; set; }
         public string createdByName { get; set; }
         public int targetId { get; set; }
+        public string narration { get; set; }
+        public decimal facilityAmount { get; set; }
     }
 }
