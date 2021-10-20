bd7d9ceecb8545a507f1892667f90b6a577b1d98
benjamin.gbaaikye@fintraksoftware.com <benjamin.gbaaikye@fintraksoftware.com>
2021-10-20 18:20:53 +0100

    propose collateral

---
+++ FintrakBanking.APICore/Reports_Access/ReportViews/DrawdownMemo.aspx.cs
@@ -0,0 +1,79 @@
+﻿using FintrakBanking.Common.Extensions;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FintrakBanking.ReportObjects;
+using FintrakBanking.ReportObjects.ReportingObjects;
+
+namespace FintrakBanking.APICore.Reports.ReportViews
+{
+    public partial class DrawdownMemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    string loanRefNo = Request.QueryString["loanRefNo"];
+                    string inputDateInfo = Request.QueryString["key1"];
+                    string inputHashValue = Request.QueryString["key2"];
+
+                    HashHelper hash = new HashHelper();
+
+                    DateTime incomingDate = DateTime.ParseExact(inputDateInfo, "ddMMyyyyHHmmss", CultureInfo.InvariantCulture);
+
+                    var incomingDateHash = hash.HashString(inputDateInfo).Replace("-", "");
+
+                    if (inputHashValue != incomingDateHash)
+                    {
+                        this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/Error.rdlc");
+                        this.ReportViewer.LocalReport.Refresh();
+                        return;
+                    }
+
+                    var currentDate = DateTime.Now;
+
+                    var dateDifference = currentDate - incomingDate;
+
+                    if (dateDifference.Seconds > 30)
+                    {
+                        this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/Error.rdlc");
+                        this.ReportViewer.LocalReport.Refresh();
+                        return;
+                    }
+
+                    DRAWDOWN drawdown = new DRAWDOWN();
+                    var data = drawdown.GetDrawdown(loanRefNo);
+                    this.ReportViewer.LocalReport.DataSources.Clear();
+                    ReportDataSource reportDataSource = new ReportDataSource();
+                    reportDataSource.Value = data;
+                    reportDataSource.Name = "drawdown";
+
+                    string exportOption = "PDF";
+                    RenderingExtension extension = ReportViewer.LocalReport.ListRenderingExtensions().ToList().Find(x => x.Name.Equals(exportOption, StringComparison.CurrentCultureIgnoreCase));
+                    if (extension != null)
+                    {
+                        System.Reflection.FieldInfo fieldInfo = extension.GetType().GetField("m_isVisible", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                        fieldInfo.SetValue(extension, false);
+                    }
+
+                    this.ReportViewer.LocalReport.DataSources.Add(reportDataSource);
+                    this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/DrawdownMemo.rdlc");
+                    ReportViewer.LocalReport.Refresh();
+                }
+                catch (Exception ex)
+                {
+                    this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/Error.rdlc");
+                    this.ReportViewer.LocalReport.Refresh();
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ FintrakBanking.APICore/Reports_Access/ReportViews/PropertyRevaluation.aspx.cs
@@ -0,0 +1,42 @@
+﻿using FintrakBanking.ReportObjects.Credit;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FintrakBanking.APICore.Reports.ReportViews
+{
+    public partial class PropertyRevaluation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoanMonitoring logic = new LoanMonitoring();
+                string companyId = Request.QueryString["companyId"];
+                string value = Request.QueryString["value"];
+                int companyIntId = 0;
+                int valueInt = 0;
+                if (!string.IsNullOrEmpty(companyId))
+                {
+                    companyIntId = Convert.ToInt32(companyId);
+                }
+                if (!string.IsNullOrEmpty(value))
+                {
+                    valueInt = Convert.ToInt32(value);
+                }
+                this.ReportViewer.LocalReport.DataSources.Clear();
+                ReportDataSource reportDataSource = new ReportDataSource();
+                reportDataSource.Value = logic.CollateralPropertyRevaluation(companyIntId,valueInt);
+                reportDataSource.Name = "revaluationReport";
+
+                this.ReportViewer.LocalReport.DataSources.Add(reportDataSource);
+                this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/CollateralRevaluation.rdlc");
+                this.ReportViewer.LocalReport.Refresh();
+            }
+        }
+    }
+}
