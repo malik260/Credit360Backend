f2d62e06ae4b5b88e4bcf75bb3c5c065b3559a77
benjamin.gbaaikye@fintraksoftware.com <benjamin.gbaaikye@fintraksoftware.com>
2021-10-21 08:22:08 +0100

    sub approval

--- FintrakBanking.ViewModels/Credit/LoanApplicationViewModel.cs
+++ FintrakBanking.ViewModels/Credit/LoanApplicationViewModel.cs
@@ -1571,5 +1571,46 @@ namespace FintrakBanking.ViewModels.Credit
         public double approvedRate { get; set; }
     }
 
+    public class HeadOfficeFacilityApprovalViewModel
+    {
+        public int loanApplicationId { get; set; }
+        public int loanApplicationDetailId { get; set; }
+        public string applicationReferenceNumber { get; set; }
+        public string relatedReferenceNumber { get; set; }
+        public int subsidiaryId { get; set; }
+        public int customerId { get; set; }
+        public int customerGlobalId { get; set; }
+        public DateTime? applicationDate { get; set; }
+        public double interestRate { get; set; }
+        public int applicationTenor { get; set; }
+        public int approvalStatusId { get; set; }
+        public int? approvalLevelId { get; set; }
+        public int approvalLevelGlobalCode { get; set; }
+        public int? toStaffId { get; set; }
+        public int applicationStatusId { get; set; }
+        public string operationName { get; set; }
+        public string productClassName { get; set; }
+        public string productName { get; set; }
+        public string productClassProcess { get; set; }
+        public string loanApplicationTypeName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string middleName { get; set; }
+        public string businessUnitShortCode { get; set; }
+        public decimal applicationAmount { get; set; }
+        public decimal totalExposureAmount { get; set; }
+        public DateTime systemDateTime { get; set; }
+        public DateTime systemArrivalDateTime { get; set; }
+        public int createdBy { get; set; }
+        public int lastUpdatedBy { get; set; }
+        public DateTime dateTimeCreated { get; set; }
+        public DateTime? dateTimeUpdated { get; set; }
+        public bool deleted { get; set; }
+        public int? deletedBy { get; set; }
+        public DateTime? dateTimeDeleted { get; set; }
+        public string responseMessage { get; set; }
+        public string responseCode { get; set; }
+    }
+
 }
 
