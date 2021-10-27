c81a5823904f47da37e9b12795db4ab1b4b8f1f0
benjamin.gbaaikye@fintraksoftware.com <benjamin.gbaaikye@fintraksoftware.com>
2021-10-27 07:59:42 +0100

    is document condition

---
+++ FintrakBanking.APICore/Reports_Wema/ReportViews/RunningLoanReport.aspx.cs
@@ -0,0 +1,78 @@
+﻿using FintrakBanking.Common.Extensions;
+using FintrakBanking.ReportObjects;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FintrakBanking.APICore.Reports.ReportViews
+{
+    public partial class RuniningLoanReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                try
+                {
+                    DateTime startDate = DateTime.ParseExact(Request.QueryString["startDate"], "dd-MM-yyyy", null);
+                    DateTime endDate = DateTime.ParseExact(Request.QueryString["endDate"], "dd-MM-yyyy", null);
+                    int companyId = Int32.Parse(Request.QueryString["companyId"]);
+                    short branchId = short.Parse(Request.QueryString["branchId"]);
+                    string inputDateInfo = Request.QueryString["key1"];
+                    string inputHashValue = Request.QueryString["key2"];
+
+                    HashHelper hash = new HashHelper();
+
+                    DateTime incomingDate = DateTime.ParseExact(inputDateInfo, "ddMMyyyyHHmmss", CultureInfo.InvariantCulture);
+
+                    var incomingDateHash = hash.HashString(inputDateInfo).Replace("-", "");
+
+                    if (inputHashValue != incomingDateHash)
+                    {
+                        this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/Error.rdlc");
+                        this.ReportViewer.LocalReport.Refresh();
+                        return;
+                    }
+
+                    var currentDate = DateTime.Now;
+
+                    var dateDifference = currentDate - incomingDate;
+
+                    if (dateDifference.Seconds > 30)
+                    {
+                        this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/Error.rdlc");
+                        this.ReportViewer.LocalReport.Refresh();
+                        return;
+                    }
+
+                    LoanReportObjects runiningLoanReport = new LoanReportObjects();
+                    var data = runiningLoanReport.RunningLoanReport(startDate, endDate, companyId, branchId);
+
+                    this.ReportViewer.LocalReport.DataSources.Clear();
+                    ReportDataSource reportDataSource = new ReportDataSource();
+                    reportDataSource.Value = data;
+                    reportDataSource.Name = "RunningLoanReport";
+
+                    //ReportParameter sDate = new ReportParameter("startDate", startDate.ToString());
+                    //ReportParameter eDate = new ReportParameter("endDate", endDate.ToString());
+
+                    this.ReportViewer.LocalReport.DataSources.Add(reportDataSource);
+                    this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/RunningLoanReport.rdlc");
+                    //ReportViewer1.LocalReport.SetParameters(new ReportParameter[] { sDate, eDate });
+                    ReportViewer.LocalReport.Refresh();
+                }
+                catch (Exception ex)
+                {
+                    this.ReportViewer.LocalReport.ReportPath = Server.MapPath("~/Reports/Report/Error.rdlc");
+                    this.ReportViewer.LocalReport.Refresh();
+                    return;
+                }
+            }
+        }
+    }
+}
