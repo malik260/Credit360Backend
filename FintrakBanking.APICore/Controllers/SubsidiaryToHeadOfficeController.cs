1e639738dd269cd18f462762b98647d6e84126a7
benjamin.gbaaikye@fintraksoftware.com <benjamin.gbaaikye@fintraksoftware.com>
2021-12-09 14:26:00 +0100

    update

--- FintrakBanking.APICore/Controllers/SubsidiaryToHeadOfficeController.cs
+++ FintrakBanking.APICore/Controllers/SubsidiaryToHeadOfficeController.cs
@@ -11,7 +11,7 @@ using System.Web.Http;
 namespace FintrakBanking.APICore.Controllers
 {
 
-    [RoutePrefix("api/v1/subsidiary")]
+    [RoutePrefix("api/v1/fintrak")]
     public class SubsidiaryToHeadOfficeController : ApiControllerBase
     {
        
