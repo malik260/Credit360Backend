d03f3869785c7f47f4060527a1e1144507cd4a83
superuser <augustine.nwaka@fintraksoftware.com>
2022-06-20 13:15:06 +0100

    dy

--- FintrakBanking.Interfaces/WorkFlow/IWorkflow.cs
+++ FintrakBanking.Interfaces/WorkFlow/IWorkflow.cs
@@ -50,7 +50,7 @@ namespace FintrakBanking.Interfaces.WorkFlow
         bool? TerminateOnApproval { set; }
         string Flow_log { set; }
         bool SkipLimitsCheck { set; }
-        int? CreditGradeId { set; }
+        //int? CreditGradeId { set; }
         //bool? IgnorePostApprovalReviewwer { set; }o
         List<WorkflowSetup> WorkflowSetup { get; }
         int? OwnerId { set; }
@@ -143,7 +143,9 @@ namespace FintrakBanking.Interfaces.WorkFlow
         public TBL_APPROVAL_BUSINESS_RULE LevelBusinessRule { get; set; }
         public bool AllowMultipleInitiator { get; set; }
         public int? RoleIdToRoute { get; set; }
+        public int? ROLEIDTOROUTE { get; set; }
         public bool IsPostApprovalReviewer { get; set; }
+        public bool ISPOSTAPPROVALREVIEWER { get; set; }
         public decimal InvestmentGradeLimit { get; set; }
         public decimal StandardGradeLimit { get; set; }
         public decimal RenewalLimit { get; set; }
