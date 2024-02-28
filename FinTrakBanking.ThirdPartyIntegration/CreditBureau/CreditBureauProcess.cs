b1c30224249445ef267fff6079edb29b4c8bc52f
mazino <chisonm.okafor@fintraksoftware.com>
2024-02-28 06:46:08 +0100

    fixes

--- FinTrakBanking.ThirdPartyIntegration/CreditBureau/CreditBureauProcess.cs
+++ FinTrakBanking.ThirdPartyIntegration/CreditBureau/CreditBureauProcess.cs
@@ -13,6 +13,7 @@
     using System.Threading.Tasks;
     using System.Xml;
     using FintrakBanking.Common.CustomException;
+    using System.Web.UI.WebControls;
 
 
     namespace CreditBureau
@@ -245,6 +246,16 @@
 
                 return str;
             }
+            private int MergeListInt(List<string> mergeId)
+            {
+                int str = 0;
+                foreach (var item in mergeId)
+                {
+                    str = Int32.Parse(item);
+                }
+
+                return str;
+            }
 
             private string DoXDSCommercialSearch(CreditBureauSearchViewModel searchInfo)
             {
@@ -325,9 +336,11 @@
                     string result = string.Empty;
                     string mergeLst = MergeListToString(searchInput.mergeList);
                     XDSService xds = new XDSService();
+                    var idString = mergeLst.TrimEnd(',');
+                    var id = Int32.Parse(idString);
                     var data = new SearchFullResultViewModel
                     {
-                        ConsumerID = searchInput.consumerID,
+                        ConsumerID = id,
                         MergeList = mergeLst.TrimEnd(','),
                         DataTicket = string.Empty,
                         EnquiryID = searchInput.enquiryID,
