e04c9746e0677d636c633453443b009240829181
malik sherifdeen <malik.sherifdeen@fintraksoftware.com>
2025-02-19 10:49:55 +0100

    ok

--- FintrakBanking.Repositories/External/LoanRepositoryExternal.cs
+++ FintrakBanking.Repositories/External/LoanRepositoryExternal.cs
@@ -2359,14 +2359,14 @@ namespace FintrakBanking.Repositories.External
                             var RefinanceMod = context.TblRefinancingLoan.Where(x => x.LoanId == Id).FirstOrDefault();
                             RefinanceMod.Checklisted = 1;
 
-                        //    var output1 = context.SaveChanges() > 0;
-                        //    trans.Commit();
-                        //    trans.Dispose();
+                            var output1 = context.SaveChanges() > 0;
+                            trans.Commit();
+                            trans.Dispose();
 
 
-                        //    return Model;
+                            return Model;
 
-                        //}
+                        }
 
                         if (ExisitngItems.Count == 0)
                         {
